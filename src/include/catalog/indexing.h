223228942f9143f37ec4d24689804061a63ba632
Daniel Gustafsson <dgustafsson@pivotal.io>
2016-03-04 11:37:27 +0100

    Fix indexing.h conflicts

--- src/include/catalog/indexing.h
+++ src/include/catalog/indexing.h
@@ -5,10 +5,7 @@
  *	  on system catalogs
  *
  *
-<<<<<<< HEAD
- * Copyright (c) 2007-2010, Greenplum inc
-=======
->>>>>>> 632e7b6353a99dd139b999efce4cb78db9a1e588
+ * Portions Copyright (c) 2007-2010, Greenplum inc
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
