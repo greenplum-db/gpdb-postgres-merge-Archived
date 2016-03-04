bf6c40e18af68132c561da6a60918b5392e1a3d8
Daniel Gustafsson <dgustafsson@pivotal.io>
2016-03-04 10:39:34 +0100

    Fix printtuple.c conflicts

--- src/backend/access/common/printtup.c
+++ src/backend/access/common/printtup.c
@@ -5,11 +5,7 @@
  *	  clients and standalone backends are supported here).
  *
  *
-<<<<<<< HEAD
  * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
-=======
- * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
->>>>>>> 632e7b6353a99dd139b999efce4cb78db9a1e588
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
