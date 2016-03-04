3beefc857e541abf847b34f83872168cd32348f4
Daniel Gustafsson <dgustafsson@pivotal.io>
2016-03-04 12:25:43 +0100

    Fix nls.mk conflicts

--- src/bin/pg_config/nls.mk
+++ src/bin/pg_config/nls.mk
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 # $PostgreSQL: pgsql/src/bin/pg_config/nls.mk,v 1.19.2.1 2009/09/03 21:01:00 petere Exp $
 CATALOG_NAME	:= pg_config
-AVAIL_LANGUAGES	:= de es fr it ja ko nb pt_BR ro ru sv ta tr
-=======
-# $PostgreSQL: pgsql/src/bin/pg_config/nls.mk,v 1.17 2007/11/15 20:38:15 petere Exp $
-CATALOG_NAME	:= pg_config
 AVAIL_LANGUAGES	:= cs de es fr ko nb pl pt_BR ro ru sl sv ta tr zh_CN zh_TW
->>>>>>> 632e7b6353a99dd139b999efce4cb78db9a1e588
 GETTEXT_FILES	:= pg_config.c ../../port/exec.c
 GETTEXT_TRIGGERS:= _
