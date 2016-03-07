538dc30f03c70b554515b7c4f4d7d1fc7d118cb6
Daniel Gustafsson <dgustafsson@pivotal.io>
2016-03-07 12:38:24 +0100

    Fix conflicts in dllist.c

--- src/backend/lib/dllist.c
+++ src/backend/lib/dllist.c
@@ -4,11 +4,7 @@
  *	  this is a simple doubly linked list implementation
  *	  the elements of the lists are void*
  *
-<<<<<<< HEAD
  * Portions Copyright (c) 1996-2009, PostgreSQL Global Development Group
-=======
- * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
->>>>>>> 632e7b6353a99dd139b999efce4cb78db9a1e588
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  *
