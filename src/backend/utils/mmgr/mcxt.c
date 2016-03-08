61f219b916567feb46ab578bd4104a934d873a8c
Daniel Gustafsson <dgustafsson@pivotal.io>
2016-03-08 13:15:31 +0100

    Drive-by fix of conflict in mcxt.c as I passed by

--- src/backend/utils/mmgr/mcxt.c
+++ src/backend/utils/mmgr/mcxt.c
@@ -9,10 +9,7 @@
  * context's MemoryContextMethods struct.
  *
  *
-<<<<<<< HEAD
  * Portions Copyright (c) 2007-2008, Greenplum inc
-=======
->>>>>>> 632e7b6353a99dd139b999efce4cb78db9a1e588
  * Portions Copyright (c) 1996-2008, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
