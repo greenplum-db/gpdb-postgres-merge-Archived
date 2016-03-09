8cbd7219282561d2481bd92705a99e7622ce8beb
Heikki Linnakangas <hlinnakangas@pivotal.io>
2016-03-09 08:03:21 +0200

    Remove dummy extentseqnum argument.
    All callers passed 1, so it didn't do anything interesting.

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -2337,7 +2337,7 @@ OpenOrCreateSubtransIdFile(DistributedTransactionId dxid)
 	{
 		subxip_file = OpenTemporaryFile(
 			Subtransaction_filename(dxid),
-			1, false, true, true, false);
+			false, true, true, false);
 		// What should be done if this fails ??
 	}
 }
@@ -2728,7 +2728,7 @@ GetSubXidsInXidBuffer(void)
 			subxip_file = OpenTemporaryFile(
 				Subtransaction_filename(
 					SharedLocalSnapshotSlot->QDxid),
-				1, false, false, false, false);
+				false, false, false, false);
 
 			if (subxip_file == 0)
 			{
@@ -2794,7 +2794,7 @@ ShowSubtransactionsForSharedSnapshot(void)
 			subxip_file = OpenTemporaryFile(
 				Subtransaction_filename(
 					SharedLocalSnapshotSlot->QDxid),
-				1, false, false, false, false);
+				false, false, false, false);
 
 			if (subxip_file == 0)
 			{
