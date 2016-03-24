b51e6584930d67fe87718b541f0886986ce65a18
Daniel Gustafsson <dgustafsson@pivotal.io>
2016-03-24 09:06:18 +0100

    Fix pg_controldata conflicts
    Nothing interesting here, only Makefile/gitignore stuff

--- src/bin/pg_controldata/nls.mk
+++ src/bin/pg_controldata/nls.mk
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 # $PostgreSQL: pgsql/src/bin/pg_controldata/nls.mk,v 1.19.2.1 2009/09/03 21:01:04 petere Exp $
 CATALOG_NAME	:= pg_controldata
-AVAIL_LANGUAGES	:= de es fr it ja ko pt_BR sv ta tr
-=======
-# $PostgreSQL: pgsql/src/bin/pg_controldata/nls.mk,v 1.17 2007/11/15 20:38:15 petere Exp $
-CATALOG_NAME	:= pg_controldata
 AVAIL_LANGUAGES	:= cs de es fa fr hu it ko nb pl pt_BR ro ru sk sl sv ta tr zh_CN zh_TW
->>>>>>> 632e7b6353a99dd139b999efce4cb78db9a1e588
 GETTEXT_FILES	:= pg_controldata.c
 GETTEXT_TRIGGERS:= _
