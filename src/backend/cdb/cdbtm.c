7fef80ec72fb40e8c34b9745a203a4172e0bbaa9
Heikki Linnakangas <hlinnakangas@pivotal.io>
2016-03-29 13:54:16 +0300

    Oops, forgot to pass an argument to function.
    That's what you get for ignoring compiler warnings.

--- src/backend/cdb/cdbtm.c
+++ src/backend/cdb/cdbtm.c
@@ -33,6 +33,7 @@
 #include "access/twophase.h"
 #include "access/distributedlog.h"
 #include "postmaster/postmaster.h"
+#include "storage/procarray.h"
 #include "cdb/cdbpersistentrecovery.h"
 #include "cdb/cdbpersistentcheck.h"
 
@@ -738,7 +739,7 @@ doInsertForgetCommitted(void)
 	 * master readers (e.g. those using  SnapshotNow for reading) the same as for
 	 * distributed transactions.
 	 */
-	ClearTransactionFromPgProc_UnderLock();
+	ClearTransactionFromPgProc_UnderLock(MyProc);
 	releaseGxact_UnderLocks();
 
 	elog(DTM_DEBUG5, "doInsertForgetCommitted called releaseGxact");
