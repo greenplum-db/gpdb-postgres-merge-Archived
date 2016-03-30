a7969f4aba448861575e9b97ecf5e0bcab23b855
Heikki Linnakangas <hlinnakangas@pivotal.io>
2016-03-30 10:52:41 +0300

    Forgot to "git add" this..

--- src/include/catalog/pg_enum.h
+++ src/include/catalog/pg_enum.h
@@ -67,7 +67,7 @@ typedef FormData_pg_enum *Form_pg_enum;
 /*
  * prototypes for functions in pg_enum.c
  */
-extern void EnumValuesCreate(Oid enumTypeOid, List *vals);
+extern void EnumValuesCreate(Oid enumTypeOid, List *vals, List **valOids);
 extern void EnumValuesDelete(Oid enumTypeOid);
 
 #endif   /* PG_ENUM_H */
