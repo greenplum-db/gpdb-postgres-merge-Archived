6f112056c0aeafac85103e77636ad38177ddd3b1
Heikki Linnakangas <hlinnakangas@pivotal.io>
2016-04-06 18:10:07 +0300

    Remove the limitation that a default must be provided for ADD COLUMN on AO table
    We had later added code that allows "DEFAULT NULL", by doing a table
    rewrite. DEFAULT NULL is really the same as no default, so we might as well
    do a table rewrite for that case too, and save the code needed to handle
    them differently.
    
    The reason to do this now is that the default_is_null flag wasn't being
    set correctly. Might as well just get rid of it, instead of debugging why.

--- src/backend/nodes/readfuncs.c
+++ src/backend/nodes/readfuncs.c
@@ -2082,7 +2082,6 @@ _readColumnDef(void)
 	READ_INT_FIELD(attnum);
 	READ_OID_FIELD(default_oid);
 	READ_NODE_FIELD(raw_default);
-	READ_BOOL_FIELD(default_is_null);
 	READ_STRING_FIELD(cooked_default);
 	READ_NODE_FIELD(constraints);
 	READ_NODE_FIELD(encoding);
