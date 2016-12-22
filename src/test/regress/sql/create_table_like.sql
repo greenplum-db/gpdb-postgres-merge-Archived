1d181301b3e633bb14f77db1b93fc5798e0c7a61
Tom Lane <tgl@sss.pgh.pa.us>
2016-12-22 16:23:34 -0500

    Cherry-pick upstream commit, to fix 'partition' test failure.
    failure:
    
    --- /tmp/build/e18b2f02/gpdb_src/src/test/regress/expected/partition.out	2019-10-15 10:26:09.936214014 +0000
    +++ /tmp/build/e18b2f02/gpdb_src/src/test/regress/results/partition.out	2019-10-15 10:26:10.172213828 +0000
    @@ -2532,8 +2532,6 @@
     ERROR:  can't drop a constraint from "rank_damage_1_prt_1"; it is part of a partitioned table
     HINT:  You may be able to perform the operation on the partitioned table as a whole.
     alter table rank_damage add partition ppo end (22) with (oids = true);
    -ERROR:  OIDS=TRUE is not allowed on partitioned tables
    -HINT:  Use OIDS=FALSE.
    
    upstream commit:
    
    commit 68330c8b42ade797995fb84f46af7549c041ad1e
    Author: Tom Lane <tgl@sss.pgh.pa.us>
    Date:   Thu Dec 22 16:23:34 2016 -0500
    
        Fix CREATE TABLE ... LIKE ... WITH OIDS.
    
        Having a WITH OIDS specification should result in the creation of an OID
        column, but commit b943f502b broke that in the case that there were LIKE
        tables without OIDS.  Commentary in that patch makes it look like this was
        intentional, but if so it was based on a faulty reading of what inheritance
        does: the parent tables can add an OID column, but they can't subtract one.
        AFAICS, the behavior ought to be that you get an OID column if any of the
        inherited tables, LIKE tables, or WITH clause ask for one.
    
        Also, revert that patch's unnecessary split of transformCreateStmt's loop
        over the tableElts list into two passes.  That seems to have been based on
        a misunderstanding as well: we already have two-pass processing here,
        we don't need three passes.
    
        Per bug #14474 from Jeff Dafoe.  Back-patch to 9.6 where the misbehavior
        was introduced.
    
        Report: https://postgr.es/m/20161222145304.25620.47445@wrigleys.postgresql.org

--- src/test/regress/sql/create_table_like.sql
+++ src/test/regress/sql/create_table_like.sql
@@ -136,6 +136,8 @@ SELECT oid FROM like_test3;
  * this case dose not make sence for us
  * CREATE TABLE like_test4 (z INTEGER, PRIMARY KEY(oid), LIKE has_oid);
  * SELECT oid FROM like_test4;
- * DROP TABLE has_oid, no_oid, like_test, like_test2, like_test3, like_test4;
  */
-DROP TABLE has_oid, no_oid, like_test, like_test2, like_test3;
+CREATE TABLE like_test5 (z INTEGER, LIKE no_oid) WITH OIDS;
+SELECT oid FROM like_test5;
+DROP TABLE has_oid, no_oid, like_test, like_test2, like_test3,
+  like_test5;
