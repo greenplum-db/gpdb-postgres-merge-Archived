2c25cb6da4a8d57f874ac1ca8fe04d64456f5246
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-09-12 19:07:38 +0300

    We already had a copy of this a few lines up.

--- src/include/port.h
+++ src/include/port.h
@@ -335,26 +335,6 @@ extern FILE *pgwin32_fopen(const char *, const char *);
 #define popen(a,b) _popen(a,b)
 #define pclose(a) _pclose(a)
 
-<<<<<<< HEAD
-=======
-/* 
- * stat() is not guaranteed to set the st_size field on win32, so we
- * redefine it to our own implementation that is.
- *
- * We must pull in sys/stat.h here so the system header definition
- * goes in first, and we redefine that, and not the other way around.
- */
-extern int pgwin32_safestat(const char *path, struct stat *buf);
-#if !defined(FRONTEND) && !defined(_DIRMOD_C)
-#include <sys/stat.h>
-#define stat(a,b) pgwin32_safestat(a,b)
-#endif
-
-/* Missing rand functions */
-extern long lrand48(void);
-extern void srand48(long seed);
-
->>>>>>> f260edb144c1e3f33d5ecc3d00d5359ab675d238
 /* New versions of MingW have gettimeofday, old mingw and msvc don't */
 #ifndef HAVE_GETTIMEOFDAY
 /* Last parameter not used */
