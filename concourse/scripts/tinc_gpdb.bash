2225bcac9cce893d7d1fdad24de07e5bf5911456
Jacob Champion <pchampion@pivotal.io>
2017-09-21 10:18:11 -0700

    --keep-going after failed ICW/TINC tests, take 2
    This reverts commit 106d5e10e76cab5d81495428d90c3a1b4251da5d, because
    that solution would have needed to be merged into the iteration branches
    in order to work properly. Copy the -k flag into the pipeline YAML so we
    can test it before polluting other branches with something that may or
    may not work correctly.

--- concourse/scripts/tinc_gpdb.bash
+++ concourse/scripts/tinc_gpdb.bash
@@ -27,7 +27,7 @@ function gen_env(){
 		export PGDATABASE="${PGDATABASE}"
 		cd ../src/test/tinc
 		source tinc_env.sh
-		make -k ${MAKE_TEST_COMMAND}
+		make ${MAKE_TEST_COMMAND}
 	EOF
 
 	chmod a+x /opt/run_test.sh
