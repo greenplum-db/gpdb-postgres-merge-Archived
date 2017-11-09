a5ece2a931db2afc6d53d9f006ab5b67d03444a8
Jacob Champion <pchampion@pivotal.io>
2017-11-09 14:18:58 -0800

    Remove duplicate ForkNumber declaration
    We've finally reached relation forks in the merge.

--- src/include/storage/relfilenode.h
+++ src/include/storage/relfilenode.h
@@ -14,30 +14,6 @@
 #ifndef RELFILENODE_H
 #define RELFILENODE_H
 
-#if 0
-// Not (yet) used in GPDB, but we might want to use it in the future.
-/*
- * The physical storage of a relation consists of one or more forks. The
- * main fork is always created, but in addition to that there can be
- * additional forks for storing various metadata. ForkNumber is used when
- * we need to refer to a specific fork in a relation.
- */
-typedef enum ForkNumber
-{
-	InvalidForkNumber = -1,
-	MAIN_FORKNUM = 0,
-	FSM_FORKNUM,
-	VISIBILITYMAP_FORKNUM
-
-	/*
-	 * NOTE: if you add a new fork, change MAX_FORKNUM below and update the
-	 * forkNames array in catalog.c
-	 */
-} ForkNumber;
-
-#define MAX_FORKNUM		VISIBILITYMAP_FORKNUM
-#endif 
-
 /*
  * The physical storage of a relation consists of one or more forks. The
  * main fork is always created, but in addition to that there can be
