ceaa95f1f193ee108bee7dffbda58f850fc5200b
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-11-16 13:46:12 +0200

    Change test table name, to avoid clashing with upstream 'constraints' test.

--- src/test/regress/sql/constraints_gp.sql
+++ src/test/regress/sql/constraints_gp.sql
@@ -8,8 +8,8 @@
 --
 SET optimizer_enable_dml_triggers = true;
 BEGIN;
-CREATE TABLE fkc_primary_table1(a int PRIMARY KEY, b text) DISTRIBUTED BY (a);
-CREATE TABLE fkc_foreign_table1(a int REFERENCES fkc_primary_table1 ON DELETE RESTRICT ON UPDATE RESTRICT, b text) DISTRIBUTED BY (a);
-INSERT INTO fkc_primary_table1 VALUES (1, 'bar');
-INSERT INTO fkc_foreign_table1 VALUES (1, 'bar');
+CREATE TABLE fkc_primary_table1_gp(a int PRIMARY KEY, b text) DISTRIBUTED BY (a);
+CREATE TABLE fkc_foreign_table1_gp(a int REFERENCES fkc_primary_table1_gp ON DELETE RESTRICT ON UPDATE RESTRICT, b text) DISTRIBUTED BY (a);
+INSERT INTO fkc_primary_table1_gp VALUES (1, 'bar');
+INSERT INTO fkc_foreign_table1_gp VALUES (1, 'bar');
 COMMIT;
