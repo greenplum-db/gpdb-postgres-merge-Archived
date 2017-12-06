b57830c632061a1e8057b53ea8632f509baa3b76
Jacob Champion <pchampion@pivotal.io>
2017-12-06 12:29:44 -0800

    Backport fixes for "PD_ALL_VISIBLE flag was incorrectly set" warnings
    Our filespace test has been running into this constantly. Two commits from
    upstream REL8_4_STABLE:
    
    commit 994940f2fb2678b89b4897d6cd7e0bfd67c07859
    Author: Heikki Linnakangas <heikki.linnakangas@iki.fi>
    Date:   Tue Mar 8 20:13:52 2011 +0200
    
        Don't throw a warning if vacuum sees PD_ALL_VISIBLE flag set on a page that
        contains newly-inserted tuples that according to our OldestXmin are not
        yet visible to everyone. The value returned by GetOldestXmin() is conservative,
        and it can move backwards on repeated calls, so if we see that contradiction
        between the PD_ALL_VISIBLE flag and status of tuples on the page, we have to
        assume it's because an earlier vacuum calculated a higher OldestXmin value,
        and all the tuples really are visible to everyone.
    
        We have received several reports of this bug, with the "PD_ALL_VISIBLE flag
        was incorrectly set in relation ..." warning appearing in logs. We were
        finally able to hunt it down with David Gould's help to run extra diagnostics
        in an environment where this happened frequently.
    
        Also reword the warning, per Robert Haas' suggestion, to not imply that the
        PD_ALL_VISIBLE flag is necessarily at fault, as it might also be a symptom
        of corruption on a tuple header.
    
        Backpatch to 8.4, where the PD_ALL_VISIBLE flag was introduced.
    
    commit 5fdab1ea516ef738a6f34fc22c989ac9902eb4d0
    Author: Heikki Linnakangas <heikki.linnakangas@iki.fi>
    Date:   Thu Sep 26 11:24:40 2013 +0300
    
        Fix spurious warning after vacuuming a page on a table with no indexes.
    
        There is a rare race condition, when a transaction that inserted a tuple
        aborts while vacuum is processing the page containing the inserted tuple.
        Vacuum prunes the page first, which normally removes any dead tuples, but
        if the inserting transaction aborts right after that, the loop after
        pruning will see a dead tuple and remove it instead. That's OK, but if the
        page is on a table with no indexes, and the page becomes completely empty
        after removing the dead tuple (or tuples) on it, it will be immediately
        marked as all-visible. That's OK, but the sanity check in vacuum would
        throw a warning because it thinks that the page contains dead tuples and
        was nevertheless marked as all-visible, even though it just vacuumed away
        the dead tuples and so it doesn't actually contain any.
    
        Spotted this while reading the code. It's difficult to hit the race
        condition otherwise, but can be done by putting a breakpoint after the
        heap_page_prune() call.
    
        Backpatch all the way to 8.4, where this code first appeared.

--- src/backend/commands/vacuumlazy.c
+++ src/backend/commands/vacuumlazy.c
@@ -503,6 +503,7 @@ lazy_scan_heap(Relation onerel, LVRelStats *vacrelstats,
 		Size		freespace;
 		bool		all_visible_according_to_vm = false;
 		bool		all_visible;
+		bool		has_dead_tuples;
 
 		/*
 		 * Skip pages that don't require vacuuming according to the visibility
@@ -668,6 +669,7 @@ lazy_scan_heap(Relation onerel, LVRelStats *vacrelstats,
 		 * requiring freezing.
 		 */
 		all_visible = true;
+		has_dead_tuples = false;
 		nfrozen = 0;
 		hastup = false;
 		prev_dead_count = vacrelstats->num_dead_tuples;
@@ -822,6 +824,7 @@ lazy_scan_heap(Relation onerel, LVRelStats *vacrelstats,
 			{
 				lazy_record_dead_tuple(vacrelstats, &(tuple.t_self));
 				tups_vacuumed += 1;
+				has_dead_tuples = true;
 			}
 			else
 			{
@@ -866,6 +869,8 @@ lazy_scan_heap(Relation onerel, LVRelStats *vacrelstats,
 		{
 			/* Remove tuples from heap */
 			lazy_vacuum_page(onerel, blkno, buf, 0, vacrelstats);
+			has_dead_tuples = false;
+
 			/* Forget the now-vacuumed tuples, and press on */
 			vacrelstats->num_dead_tuples = 0;
 			vacuumed_pages++;
@@ -879,9 +884,22 @@ lazy_scan_heap(Relation onerel, LVRelStats *vacrelstats,
 			PageSetAllVisible(page);
 			MarkBufferDirty(buf);
 		}
-		else if (PageIsAllVisible(page) && !all_visible)
+		/*
+		 * It's possible for the value returned by GetOldestXmin() to move
+		 * backwards, so it's not wrong for us to see tuples that appear to
+		 * not be visible to everyone yet, while PD_ALL_VISIBLE is already
+		 * set. The real safe xmin value never moves backwards, but
+		 * GetOldestXmin() is conservative and sometimes returns a value
+		 * that's unnecessarily small, so if we see that contradiction it
+		 * just means that the tuples that we think are not visible to
+		 * everyone yet actually are, and the PD_ALL_VISIBLE flag is correct.
+		 *
+		 * There should never be dead tuples on a page with PD_ALL_VISIBLE
+		 * set, however.
+		 */
+		else if (PageIsAllVisible(page) && has_dead_tuples)
 		{
-			elog(WARNING, "PD_ALL_VISIBLE flag was incorrectly set in relation \"%s\" page %u",
+			elog(WARNING, "page containing dead tuples is marked as all-visible in relation \"%s\" page %u",
 				 relname, blkno);
 			PageClearAllVisible(page);
 			MarkBufferDirty(buf);
