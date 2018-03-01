c01cdb7851ef85f937eaa011b3731eab723abaad
Heikki Linnakangas <hlinnakangas@pivotal.io>
2018-03-01 10:41:15 +0800

    Fix a few misc merge conflicts in comments and docs.

--- src/bin/psql/tab-complete.c
+++ src/bin/psql/tab-complete.c
@@ -1,11 +1,8 @@
 /*
  * psql - the PostgreSQL interactive terminal
  *
-<<<<<<< HEAD
  * Portions Copyright (c) 2005-2010, Greenplum inc
  * Portions Copyright (c) 2012-Present Pivotal Software, Inc.
-=======
->>>>>>> 1084f317702e1a039696ab8a37caf900e55ec8f2
  * Copyright (c) 2000-2010, PostgreSQL Global Development Group
  *
  * $PostgreSQL: pgsql/src/bin/psql/tab-complete.c,v 1.200 2010/07/06 19:19:00 momjian Exp $
