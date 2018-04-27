bb1ba8a8a0541c1e91849efd8f2427f25e571a5a
Asim R P <apraveen@pivotal.io>
2018-04-27 16:41:43 -0700

    Use REPEATABLE READ for reindex_with_insert_heap

--- src/test/isolation2/sql/reindex/repeatable_read_reindex_with_insert_heap.sql
+++ src/test/isolation2/sql/reindex/repeatable_read_reindex_with_insert_heap.sql
@@ -13,7 +13,7 @@ create index idxg_reindex_serialize_tab_heap on reindex_serialize_tab_heap(g);
 SET gp_create_table_random_default_distribution=off;
 -- end_ignore
 1: BEGIN;
-1: SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+1: SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 1: select 'dummy select to establish snapshot';
 2: BEGIN;
 2: insert into reindex_serialize_tab_heap values(99,'green',now(),10,15.10);
