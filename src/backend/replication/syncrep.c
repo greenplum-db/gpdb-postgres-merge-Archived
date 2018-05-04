greenplum-db/gpdb-postgres-merge-Archived

commit 80ac1638d044df25ffb189324d663e0393cb6793

Fix hangs in SyncRepWaitForLSN() for master
Sessions can hang on the master node if the wal sender process
replicates an LSN before a backend calls SyncRepWaitForLSN. This was
due to a regression introduced when resolving diffs in syncrep.c.

Co-authored-by: Jacob Champion <pchampion@pivotal.io>

src/backend/replication/syncrep.c
@@ -190,7 +190,7 @@ SyncRepWaitForLSN(XLogRecPtr XactCommitLSN)
 	 * condition but we'll be fetching that cache line anyway so its likely to
 	 * be a low cost check.
 	 */
-	else if (!WalSndCtl->sync_standbys_defined ||
+	if (!WalSndCtl->sync_standbys_defined ||
 		XLByteLE(XactCommitLSN, WalSndCtl->lsn[mode]))
 	{
 		elogif(debug_walrepl_syncrep, LOG,

commit 5d55b55f436c3746ece9871cc3e1c519e362370c

Fix syncrep regression
Fixes syncrep regression introduced in 80ac1638. The value of
sync_standbys_defined is only initialized/used on QEs. It is not
defined on a master.

Co-authored-by: Jacob Champion <pchampion@pivotal.io>

src/backend/replication/syncrep.c
@@ -190,7 +190,7 @@ SyncRepWaitForLSN(XLogRecPtr XactCommitLSN)
 	 * condition but we'll be fetching that cache line anyway so its likely to
 	 * be a low cost check.
 	 */
-	if (!WalSndCtl->sync_standbys_defined ||
+	if (((GpIdentity.segindex != MASTER_CONTENT_ID) && !WalSndCtl->sync_standbys_defined) ||
 		XLByteLE(XactCommitLSN, WalSndCtl->lsn[mode]))
 	{
 		elogif(debug_walrepl_syncrep, LOG,