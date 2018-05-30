bd8ad3ea97eef055bf0940723ee822443fbfc490
Richard Guo <guofenglinux@gmail.com>
2018-05-30 09:09:41 +0000

    Resolved conflicts in prep/preptlist.c

--- src/backend/optimizer/prep/preptlist.c
+++ src/backend/optimizer/prep/preptlist.c
@@ -13,13 +13,9 @@
  * between here and there is a bit arbitrary and historical.
  *
  *
-<<<<<<< HEAD
  * Portions Copyright (c) 2006-2008, Greenplum inc
  * Portions Copyright (c) 2012-Present Pivotal Software, Inc.
- * Portions Copyright (c) 1996-2011, PostgreSQL Global Development Group
-=======
  * Portions Copyright (c) 1996-2012, PostgreSQL Global Development Group
->>>>>>> 80edfd76591fdb9beec061de3c05ef4e9d96ce56
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * IDENTIFICATION
