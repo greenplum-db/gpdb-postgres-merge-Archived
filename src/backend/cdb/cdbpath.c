68ea7801465246e83fbd6d72f3499705b4e4c67e
Paul Guo <paulguo@gmail.com>
2018-06-05 16:22:49 +0800

    Fix compilation errors in cdbpath.c

--- src/backend/cdb/cdbpath.c
+++ src/backend/cdb/cdbpath.c
@@ -1477,7 +1477,10 @@ cdbpath_dedup_fixup_baserel(Path *path, CdbpathDedupFixupContext *ctx)
 	}
 
 	/* Add these vars to the rel's list of result columns. */
-	add_vars_to_targetlist(ctx->root, rowid_vars, ctx->distinct_on_rowid_relids);
+	/* GPDB_92_MERGE_FIXME: set create_new_ph as true or false?
+	 * I'm setting it as safe value true here.
+	 */
+	add_vars_to_targetlist(ctx->root, rowid_vars, ctx->distinct_on_rowid_relids, true);
 
 	/* Recalculate width of the rel's result rows. */
 	set_rel_width(ctx->root, rel);
