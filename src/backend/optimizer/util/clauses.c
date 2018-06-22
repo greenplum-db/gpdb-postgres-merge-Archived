greenplum-db/gpdb-postgres-merge-Archived

commit 260ec4012a212a817e8eb5eb30d0613dad513b19

Remove the compile errors which is in optimizer

src/backend/optimizer/util/clauses.c
@@ -2257,12 +2257,13 @@ rowtype_field_matches(Oid rowtypeid, int fieldnum,
  *
  * Recurses into query tree and folds all constant expressions.
  */
+
 Query *
-fold_constants(PlannerGlobal *glob, Query *q, ParamListInfo boundParams, Size max_size)
+fold_constants(PlannerInfo *root, Query *q, ParamListInfo boundParams, Size max_size)
 {
 	eval_const_expressions_context context;
 
-	context.glob = glob;
+	context.root = root;
 	context.boundParams = boundParams;
 	context.active_fns = NIL;	/* nothing being recursively simplified */
 	context.case_val = NULL;	/* no CASE being examined */
@@ -2493,7 +2494,7 @@ eval_const_expressions_mutator(Node *node,
 							 * win.
 							 */
 							if (!(context->estimate || (prm->pflags & PARAM_FLAG_CONST)))
-								context->glob->oneoffPlan = true;
+								context->root->glob->oneoffPlan = true;
 
 							Assert(prm->ptype == param->paramtype);
 							get_typlenbyval(param->paramtype,
@@ -2541,6 +2542,7 @@ eval_const_expressions_mutator(Node *node,
 										   &args,
 										   true,
 										   true,
+										   true,
 										   context);
 				if (simple)		/* successfully simplified it */
 					return (Node *) simple;
@@ -2584,6 +2586,7 @@ eval_const_expressions_mutator(Node *node,
 										   expr->opcollid,
 										   expr->inputcollid,
 										   &args,
+										   true,
 										   true,
 										   true,
 										   context);
@@ -2687,6 +2690,7 @@ eval_const_expressions_mutator(Node *node,
 											   expr->opcollid,
 											   expr->inputcollid,
 											   &args,
+											   false,
 											   false,
 											   false,
 											   context);
@@ -2892,6 +2896,7 @@ eval_const_expressions_mutator(Node *node,
 										   &args,
 										   true,
 										   true,
+										   true,
 										   context);
 				if (simple)		/* successfully simplified output fn */
 				{
@@ -2922,6 +2927,7 @@ eval_const_expressions_mutator(Node *node,
 											   InvalidOid,
 											   &args,
 											   false,
+											   true,
 											   true,
 											   context);
 					if (simple) /* successfully simplified input fn */
@@ -3813,12 +3819,16 @@ simplify_boolean_equality(Oid opno, List *args)
  * will be done even if simplification of the function call itself is not
  * possible.
  */
+/*
+ * GPDB_92_MERGE_FIXME: please check if the arguments input by whom is
+ * called this function is correct,?
+ */
 static Expr *simplify_function(Oid funcid,
 				  Oid result_type, int32 result_typmod,
 				  Oid result_collid, Oid input_collid, List **args_p,
 				  bool funcvariadic, 
 				  bool process_args, bool allow_non_const,
-				  eval_const_expressions_context *context);
+				  eval_const_expressions_context *context)
 {
 	List	   *args = *args_p;
 	HeapTuple	func_tuple;
@@ -4235,10 +4245,10 @@ evaluate_function(Oid funcid, Oid result_type, int32 result_typmod,
 		 /* okay */ ;
 	else if (context->estimate && funcform->provolatile == PROVOLATILE_STABLE)
 		 /* okay */ ;
-	else if (context->glob && funcform->provolatile == PROVOLATILE_STABLE)
+	else if (context->root->glob && funcform->provolatile == PROVOLATILE_STABLE)
 	{
 		 /* okay, but we cannot reuse this plan */
-		context->glob->oneoffPlan = true;
+		context->root->glob->oneoffPlan = true;
 	}
 	else
 		return NULL;

src/backend/optimizer/plan/plangroupext.c
@@ -1700,9 +1700,7 @@ plan_append_aggs_with_rewrite(PlannerInfo *root,
 			agg_plan = (Plan *)make_subqueryscan(subquery_tlist,
 												 NIL,
 												 list_length(final_query->rtable),
-												 agg_plan,
-												 final_query->rtable,
-												 final_query->rowMarks);
+												 agg_plan);
 			mark_passthru_locus(agg_plan, true, true);
 			pfree(resno_map);
 		}
@@ -2331,9 +2329,7 @@ plan_list_rollup_plans(PlannerInfo *root,
 			rollup_plan = (Plan *) make_subqueryscan(subquery_tlist,
 													 NIL,
 													 list_length(final_query->rtable),
-													 rollup_plan,
-													 root->parse->rtable,
-													 root->parse->rowMarks);
+													 rollup_plan);
 			mark_passthru_locus(rollup_plan, true, true);
 			pfree(resno_map);
 		}
@@ -2644,7 +2640,7 @@ add_repeat_node(Plan *result_plan, int repeat_count, uint64 grouping)
 	}
 	else
 	{
-		repeatCountExpr = (Expr *)makeVar(OUTER,
+		repeatCountExpr = (Expr *)makeVar(OUTER_VAR,
 										  repeat_count_resno,
 										  INT4OID,
 										  -1,

src/backend/optimizer/plan/planner.c
@@ -40,6 +40,7 @@
 #include "optimizer/transform.h"
 #include "optimizer/tlist.h"
 #include "parser/analyze.h"
+#include "parser/parse_oper.h"
 #include "parser/parse_relation.h"
 #include "parser/parsetree.h"
 #include "rewrite/rewriteManip.h"
@@ -341,7 +342,6 @@ standard_planner(Query *parse, int cursorOptions, ParamListInfo boundParams)
 	{
 		Plan	   *subplan = (Plan *) lfirst(lp);
 		PlannerInfo	   *subroot = (PlannerInfo *) lfirst(lr);
-		List       *subrtable;
 
 		List       *subrtable= subroot->parse->rtable;
 
@@ -2455,7 +2455,8 @@ grouping_planner(PlannerInfo *root, double tuple_fraction)
 	 */
 	if ((parse->distinctClause || parse->sortClause) &&
 		(root->config->honor_order_by || !root->parent_root) &&
-		!parse->intoClause &&
+		/*GPDB_92_MERGE_FIXME: pg92 change a way to implement the SELECT...INTO...*/
+		/*!parse->intoClause &&*/
 		/*
 		 * GPDB_84_MERGE_FIXME: Does this do the right thing, if you have a
 		 * SELECT DISTINCT query as argument to a table function?
@@ -3896,6 +3897,7 @@ make_subplanTargetList(PlannerInfo *root,
 	List	   *sub_tlist;
 	List	   *non_group_cols;
 	List	   *non_group_vars;
+	List	   *extravars;
 	int			numCols;
 
 	*groupColIdx = NULL;
@@ -3974,6 +3976,8 @@ make_subplanTargetList(PlannerInfo *root,
 	 */
 	if (numCols > 0)
 	{
+		int			keyno = 0;
+
 		/*
 		 * If grouping, create sub_tlist entries for all GROUP BY columns, and
 		 * make an array showing where the group columns are in the sub_tlist.

src/backend/optimizer/plan/planpartition.c
@@ -504,6 +504,7 @@ add_restrictinfos(PlannerInfo *root, DynamicScanInfo *dsinfo, Bitmapset *childre
 								  true,
 								  NULL,
 								  NULL,
+								  NULL,
 								  NULL);
 
 		relinfo->baserestrictinfo = lappend(relinfo->baserestrictinfo, rinfo);
@@ -526,7 +527,7 @@ make_mergeclause(Node *outer, Node *inner)
 
 	xpr = make_notclause((Expr *) opxpr);
 
-	rinfo = make_restrictinfo(xpr, false, false, false, NULL, NULL, NULL);
+	rinfo = make_restrictinfo(xpr, false, false, false, NULL, NULL, NULL, NULL);
 	rinfo->mergeopfamilies = get_mergejoin_opfamilies(opxpr->opno);
 
 	return rinfo;

src/backend/optimizer/plan/setrefs.c
@@ -131,12 +131,12 @@ static List *fix_join_expr(PlannerInfo *root,
 			  Index acceptable_rel, int rtoffset);
 static Node *fix_join_expr_mutator(Node *node,
 					  fix_join_expr_context *context);
-static List *fix_hashclauses(PlannerGlobal *glob,
+static List *fix_hashclauses(PlannerInfo *root,
 				List *clauses,
 				indexed_tlist *outer_itlist,
 				indexed_tlist *inner_itlist,
 				Index acceptable_rel, int rtoffset);
-static List *fix_child_hashclauses(PlannerGlobal *glob,
+static List *fix_child_hashclauses(PlannerInfo *root,
 					  List *clauses,
 					  indexed_tlist *outer_itlist,
 					  indexed_tlist *inner_itlist,
@@ -481,9 +481,10 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
         indexed_tlist  *plan_itlist = build_tlist_index(plan->targetlist);
 
         plan->flow->hashExpr =
-		(List *)fix_upper_expr(glob,
+		(List *)fix_upper_expr(root,
 							   (Node *)plan->flow->hashExpr,
 							   plan_itlist,
+							   OUTER_VAR,
 							   rtoffset);
         pfree(plan_itlist);
     }
@@ -501,14 +502,14 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 				Scan    *splan = (Scan *) plan;
 
 				if (cdb_expr_requires_full_eval((Node *)plan->targetlist))
-					return cdb_insert_result_node(glob, plan, rtoffset);
+					return cdb_insert_result_node(root, plan, rtoffset);
 
 				splan->scanrelid += rtoffset;
 
 				/* If the scan appears below a shareinput, we hit this assert. */
 #ifdef USE_ASSERT_CHECKING
-				Assert(splan->scanrelid <= list_length(glob->finalrtable) && "Scan node's relid is outside the finalrtable!");
-				RangeTblEntry *rte = rt_fetch(splan->scanrelid, glob->finalrtable);
+				Assert(splan->scanrelid <= list_length(root->glob->finalrtable) && "Scan node's relid is outside the finalrtable!");
+				RangeTblEntry *rte = rt_fetch(splan->scanrelid, root->glob->finalrtable);
 				Assert((rte->rtekind == RTE_RELATION || rte->rtekind == RTE_CTE) && "Scan plan should refer to a scan relation");
 #endif
 
@@ -523,7 +524,7 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 				IndexScan  *splan = (IndexScan *) plan;
 
 				if (cdb_expr_requires_full_eval((Node *)plan->targetlist))
-					return cdb_insert_result_node(glob, plan, rtoffset);
+					return cdb_insert_result_node(root->glob, plan, rtoffset);
 
 				splan->scan.scanrelid += rtoffset;
 
@@ -574,7 +575,7 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 				BitmapHeapScan *splan = (BitmapHeapScan *) plan;
 
 				if (cdb_expr_requires_full_eval((Node *)plan->targetlist))
-					return cdb_insert_result_node(glob, plan, rtoffset);
+					return cdb_insert_result_node(root->glob, plan, rtoffset);
 
 				splan->scan.scanrelid += rtoffset;
 
@@ -585,11 +586,11 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 					   relstorage != RELSTORAGE_AOCOLS);
 #endif
 				splan->scan.plan.targetlist =
-					fix_scan_list(glob, splan->scan.plan.targetlist, rtoffset);
+					fix_scan_list(root, splan->scan.plan.targetlist, rtoffset);
 				splan->scan.plan.qual =
-					fix_scan_list(glob, splan->scan.plan.qual, rtoffset);
+					fix_scan_list(root, splan->scan.plan.qual, rtoffset);
 				splan->bitmapqualorig =
-					fix_scan_list(glob, splan->bitmapqualorig, rtoffset);
+					fix_scan_list(root, splan->bitmapqualorig, rtoffset);
 			}
 			break;
 		case T_BitmapAppendOnlyScan:
@@ -597,7 +598,7 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 				BitmapAppendOnlyScan *splan = (BitmapAppendOnlyScan *) plan;
 
 				if (cdb_expr_requires_full_eval((Node *)plan->targetlist))
-					return cdb_insert_result_node(glob, plan, rtoffset);
+					return cdb_insert_result_node(root->glob, plan, rtoffset);
 
 				splan->scan.scanrelid += rtoffset;
 
@@ -609,11 +610,11 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 #endif
 
 				splan->scan.plan.targetlist =
-					fix_scan_list(glob, splan->scan.plan.targetlist, rtoffset);
+					fix_scan_list(root, splan->scan.plan.targetlist, rtoffset);
 				splan->scan.plan.qual =
-					fix_scan_list(glob, splan->scan.plan.qual, rtoffset);
+					fix_scan_list(root, splan->scan.plan.qual, rtoffset);
 				splan->bitmapqualorig =
-					fix_scan_list(glob, splan->bitmapqualorig, rtoffset);
+					fix_scan_list(root, splan->bitmapqualorig, rtoffset);
 			}
 			break;
 		case T_BitmapTableScan:
@@ -621,7 +622,7 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 				BitmapTableScan *splan = (BitmapTableScan *) plan;
 
 				if (cdb_expr_requires_full_eval((Node *)plan->targetlist))
-					return cdb_insert_result_node(glob, plan, rtoffset);
+					return cdb_insert_result_node(root->glob, plan, rtoffset);
 
 				splan->scan.scanrelid += rtoffset;
 
@@ -638,13 +639,13 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 				TidScan    *splan = (TidScan *) plan;
 
 				if (cdb_expr_requires_full_eval((Node *)plan->targetlist))
-					return cdb_insert_result_node(glob, plan, rtoffset);
+					return cdb_insert_result_node(root->glob, plan, rtoffset);
 
 				splan->scan.scanrelid += rtoffset;
 
 #ifdef USE_ASSERT_CHECKING
 				/* We only support TidScans on heap tables currently */
-				RangeTblEntry *rte = rt_fetch(splan->scan.scanrelid, glob->finalrtable);
+				RangeTblEntry *rte = rt_fetch(splan->scan.scanrelid, root->glob->finalrtable);
 				char relstorage = get_rel_relstorage(rte->relid);
 				Assert(relstorage == RELSTORAGE_HEAP);
 #endif
@@ -660,7 +661,7 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 		case T_SubqueryScan:
 
 			if (cdb_expr_requires_full_eval((Node *)plan->targetlist))
-				return cdb_insert_result_node(glob, plan, rtoffset);
+				return cdb_insert_result_node(root->glob, plan, rtoffset);
 
 			/* Needs special treatment, see comments below */
 			return set_subqueryscan_references(root,
@@ -673,13 +674,12 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 				List	   *subrtable = tplan->subrtable;
 
 				if (cdb_expr_requires_full_eval((Node *)plan->targetlist))
-					return cdb_insert_result_node(glob, plan, rtoffset);
+					return cdb_insert_result_node(root->glob, plan, rtoffset);
 
 				/* recursively process the subplan */
 				/* GPDB_90_MERGE_FIXME: How about rowmarks here? Do we need to stash them
 				 * in TableFunctionScan? */
-				plan->lefttree = set_plan_references(glob, subplan,
-													 subrtable, NIL);
+				plan->lefttree = set_plan_references(root, subplan);
 
 				/* subrtable is no longer needed in the plan tree */
 				tplan->subrtable = NIL;
@@ -687,9 +687,9 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 				/* adjust for the new range table offset */
 				tplan->scan.scanrelid += rtoffset;
 				tplan->scan.plan.targetlist =
-					fix_scan_list(glob, tplan->scan.plan.targetlist, rtoffset);
+					fix_scan_list(root, tplan->scan.plan.targetlist, rtoffset);
 				tplan->scan.plan.qual =
-					fix_scan_list(glob, tplan->scan.plan.qual, rtoffset);
+					fix_scan_list(root, tplan->scan.plan.qual, rtoffset);
 
 				return plan;
 			}
@@ -698,7 +698,7 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 				FunctionScan *splan = (FunctionScan *) plan;
 
 				if (cdb_expr_requires_full_eval((Node *)plan->targetlist))
-					return cdb_insert_result_node(glob, plan, rtoffset);
+					return cdb_insert_result_node(root->glob, plan, rtoffset);
 
 				splan->scan.scanrelid += rtoffset;
 				splan->scan.plan.targetlist =
@@ -714,7 +714,7 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 				ValuesScan *splan = (ValuesScan *) plan;
 
 				if (cdb_expr_requires_full_eval((Node *)plan->targetlist))
-					return cdb_insert_result_node(glob, plan, rtoffset);
+					return cdb_insert_result_node(root->glob, plan, rtoffset);
 
 				splan->scan.scanrelid += rtoffset;
 				splan->scan.plan.targetlist =
@@ -765,7 +765,7 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 		case T_MergeJoin:
 		case T_HashJoin:
 			if (cdb_expr_requires_full_eval((Node *)plan->targetlist))
-				return cdb_insert_result_node(glob, plan, rtoffset);
+				return cdb_insert_result_node(root->glob, plan, rtoffset);
 			set_join_references(root, (Join *) plan, rtoffset);
 			break;
 		case T_Plan:
@@ -815,7 +815,7 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 
 					Assert(sisc->share_type != SHARE_NOTSHARED);
 					Assert(sisc->share_id >= 0 && sisc->share_id < glob->share.producer_count);
-					producer = glob->share.producers[sisc->share_id];
+					producer = root->glob->share.producers[sisc->share_id];
 					childPlan = producer->scan.plan.lefttree;
 				}
 
@@ -844,23 +844,23 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 				indexed_tlist *childplan_itlist =
 					build_tlist_index(plan->lefttree->targetlist);
 
-				set_upper_references(glob, plan, rtoffset);
+				set_upper_references(root, plan, rtoffset);
 
 				//set_dummy_tlist_references(plan, rtoffset);
 				Assert(ps->plan.qual == NIL);
 
 				ps->levelEqExpressions = (List *)
-					fix_upper_expr(glob, (Node *) ps->levelEqExpressions, childplan_itlist, rtoffset);
+					fix_upper_expr(root, (Node *) ps->levelEqExpressions, childplan_itlist, OUTER_VAR,rtoffset);
 				ps->levelExpressions = (List *)
-					fix_upper_expr(glob, (Node *) ps->levelExpressions, childplan_itlist, rtoffset);
+					fix_upper_expr(root, (Node *) ps->levelExpressions, childplan_itlist, OUTER_VAR,rtoffset);
 				ps->residualPredicate =
-					fix_upper_expr(glob, ps->residualPredicate, childplan_itlist, rtoffset);
+					fix_upper_expr(root, ps->residualPredicate, childplan_itlist, OUTER_VAR,rtoffset);
 				ps->propagationExpression =
-					fix_upper_expr(glob, ps->propagationExpression, childplan_itlist, rtoffset);
+					fix_upper_expr(root, ps->propagationExpression, childplan_itlist, OUTER_VAR,rtoffset);
 				ps->printablePredicate =
-					fix_upper_expr(glob, ps->printablePredicate, childplan_itlist, rtoffset);
+					fix_upper_expr(root, ps->printablePredicate, childplan_itlist, OUTER_VAR,rtoffset);
 				ps->partTabTargetlist = (List *)
-					fix_upper_expr(glob, (Node *) ps->partTabTargetlist, childplan_itlist, rtoffset);
+					fix_upper_expr(root, (Node *) ps->partTabTargetlist, childplan_itlist, OUTER_VAR,rtoffset);
 			}
 			break;
 			
@@ -905,7 +905,7 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 			}
 			break;
 		case T_Agg:
-			set_upper_references(glob, plan, rtoffset);
+			set_upper_references(root, plan, rtoffset);
 			break;
 		case T_WindowAgg:
 			{
@@ -929,10 +929,10 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 
 					wplan->startOffset =
 						fix_upper_expr(root, wplan->startOffset,
-									   subplan_itlist, rtoffset);
+									   subplan_itlist, OUTER_VAR, rtoffset);
 					wplan->endOffset =
 						fix_upper_expr(root, wplan->endOffset,
-									   subplan_itlist, rtoffset);
+									   subplan_itlist, OUTER_VAR, rtoffset);
 					pfree(subplan_itlist);
 				}
 			}
@@ -961,7 +961,7 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 			}
 			break;
 		case T_Repeat:
-			set_upper_references(glob, plan, rtoffset);
+			set_upper_references(root, plan, rtoffset);
 			break;
 		case T_ModifyTable:
 			{
@@ -1120,7 +1120,7 @@ set_plan_refs(PlannerInfo *root, Plan *plan, int rtoffset)
 					build_tlist_index(plan->lefttree->targetlist);
 
 				motion->hashExpr = (List *)
-					fix_upper_expr(glob, (Node*) motion->hashExpr, childplan_itlist, rtoffset);
+					fix_upper_expr(root, (Node*) motion->hashExpr, childplan_itlist,  OUTER_VAR, rtoffset);
 
 #ifdef USE_ASSERT_CHECKING
 				/* 1. Assert that the Motion node has same number of hash data types as that of hash expressions*/
@@ -1513,7 +1513,7 @@ fix_scan_expr_mutator(Node *node, fix_scan_expr_context *context)
             Node 				   *exprCopy = NULL;
             
             /* Look up the pseudo column definition. */
-            rte = rt_fetch(var->varno, context->glob->finalrtable);
+            rte = rt_fetch(var->varno, context->root->glob->finalrtable);
             rci = cdb_rte_find_pseudo_column(rte, var->varattno);
             Assert(rci && rci->defexpr && "No expression for pseudo column");
 
@@ -1645,7 +1645,7 @@ set_join_references(PlannerInfo *root, Join *join, int rtoffset)
 										(Index) 0,
 										rtoffset);
 
-		hj->hashqualclauses = fix_join_expr(glob,
+		hj->hashqualclauses = fix_join_expr(root,
 											hj->hashqualclauses,
 											outer_itlist,
 											inner_itlist,
@@ -1710,15 +1710,16 @@ set_upper_references(PlannerInfo *root, Plan *plan, int rtoffset)
 					search_indexed_tlist_for_sortgroupref((Node *) tle->expr,
 														  tle->ressortgroupref,
 														  subplan_itlist,
-														  OUTER);
+														  OUTER_VAR);
 				if (!newexpr)
-					newexpr = fix_upper_expr(glob,
+					newexpr = fix_upper_expr(root,
 											 (Node *) tle->expr,
 											 subplan_itlist,
+											 OUTER_VAR,
 											 rtoffset);
 			}
 			else
-				newexpr = fix_upper_expr(glob,
+				newexpr = fix_upper_expr(root,
 										 (Node *) tle->expr,
 										 subplan_itlist,
 										 OUTER_VAR,
@@ -2076,7 +2077,7 @@ fix_join_expr(PlannerInfo *root,
  *  target entries found in the target list of join's outer child
  *
  */
-static List *fix_hashclauses(PlannerGlobal *glob,
+static List *fix_hashclauses(PlannerInfo *root,
                            List *clauses,
                            indexed_tlist *outer_itlist,
                            indexed_tlist *inner_itlist,
@@ -2099,33 +2100,33 @@ static List *fix_hashclauses(PlannerGlobal *glob,
          * in join's inner child target list
          * we change walker's context to guarantee this
          */
-        List *new_outer_arg = fix_child_hashclauses(glob,
+        List *new_outer_arg = fix_child_hashclauses(root,
                 outer_arg,
                 outer_itlist,
                 inner_itlist,
                 (Index) 0,
                 rtoffset,
-                OUTER);
+                OUTER_VAR);
         /*
          * for inner argument, we cannot refer to target entries
          * in join's outer child target list, otherwise hash table
          * creation could fail,
          * we change walker's context to guarantee this
          */
-        List *new_inner_arg = fix_child_hashclauses(glob,
+        List *new_inner_arg = fix_child_hashclauses(root,
                 inner_arg,
                 outer_itlist,
                 inner_itlist,
                 (Index) 0,
                 rtoffset,
-                INNER);
+                INNER_VAR);
         new_args = lappend(new_args, new_outer_arg);
         new_args = lappend(new_args, new_inner_arg);
         /* replace old arguments with the fixed arguments */
         list_free(opexpr->args);
         opexpr->args = new_args;
         /* fix opexpr */
-        fix_expr_common(glob, node);
+        fix_expr_common(root, node);
     }
     return clauses;
 }
@@ -2145,7 +2146,7 @@ static List *fix_hashclauses(PlannerGlobal *glob,
  *
  */
 static List *
-fix_child_hashclauses(PlannerGlobal *glob,
+fix_child_hashclauses(PlannerInfo *root,
               List *clauses,
               indexed_tlist *outer_itlist,
               indexed_tlist *inner_itlist,
@@ -2154,12 +2155,12 @@ fix_child_hashclauses(PlannerGlobal *glob,
               Index child)
 {
     fix_join_expr_context context;
-    context.glob = glob;
+    context.root = root;
     context.outer_itlist = outer_itlist;
     context.inner_itlist = inner_itlist;
     context.acceptable_rel = acceptable_rel;
     context.rtoffset = rtoffset;
-    if (INNER == child)
+    if (INNER_VAR == child)
     {
     	/* skips using outer target list when matching non-vars */
     	context.use_outer_tlist_for_matching_nonvars = false;

src/backend/optimizer/prep/prepjointree.c
@@ -521,6 +521,7 @@ pull_up_sublinks_qual_recurse(PlannerInfo *root, Node *node,
 												   available_rels1);
 				if (subst && IsA(subst, JoinExpr))
 				{
+					j = (JoinExpr *) subst;
 					/* Yes; insert the new join node into the join tree */
 					j->larg = *jtlink1;
 					*jtlink1 = (Node *) j;
@@ -552,6 +553,7 @@ pull_up_sublinks_qual_recurse(PlannerInfo *root, Node *node,
 												   available_rels2);
 					if (subst && IsA(subst, JoinExpr))
 					{
+						j = (JoinExpr *) subst;
 						/* Yes; insert the new join node into the join tree */
 						j->larg = *jtlink2;
 						*jtlink2 = (Node *) j;
@@ -593,7 +595,7 @@ pull_up_sublinks_qual_recurse(PlannerInfo *root, Node *node,
 			{
 				sublink->subLinkType = (sublink->subLinkType == ANY_SUBLINK) ? ALL_SUBLINK : ANY_SUBLINK;
 				sublink->testexpr = (Node *) canonicalize_qual(make_notclause((Expr *) sublink->testexpr));
-				return pull_up_sublinks_qual_recurse(root, (Node *) sublink, available_rels, jtlink);
+				return pull_up_sublinks_qual_recurse(root, (Node *) sublink, available_rels1, jtlink1, available_rels2, jtlink2);
 			}
 
 			/*
@@ -608,7 +610,7 @@ pull_up_sublinks_qual_recurse(PlannerInfo *root, Node *node,
 			/* NOT NOT (expr) => (expr)  */
 			return (Node *) pull_up_sublinks_qual_recurse(root,
 														 (Node *) get_notclausearg((Expr *) arg),
-														 available_rels, jtlink);
+														 available_rels1, jtlink1, available_rels2, jtlink2);
 		}
 		/*
 		 * GPDB_91_MERGE_FIXME: The below enters an infinite recursion with
@@ -685,8 +687,8 @@ pull_up_sublinks_qual_recurse(PlannerInfo *root, Node *node,
 				if (j)
 				{
 					/* Yes, insert the new join node into the join tree */
-					j->larg = *jtlink;
-					*jtlink = (Node *) j;
+					j->larg = *jtlink1;
+					*jtlink1 = (Node *) j;
 				}
 				return node;
 			}

commit 8543bf893fb08e91e5083d44993249ccca8f854c

Fix copy2 failure
commit 260ec4012a2 introduced a bug:
accessing eval_const_expressions_context->root->glob, where
eval_const_expressions_context->root could be NULL

src/backend/optimizer/util/clauses.c
@@ -4245,7 +4245,7 @@ evaluate_function(Oid funcid, Oid result_type, int32 result_typmod,
 		 /* okay */ ;
 	else if (context->estimate && funcform->provolatile == PROVOLATILE_STABLE)
 		 /* okay */ ;
-	else if (context->root->glob && funcform->provolatile == PROVOLATILE_STABLE)
+	else if (context->root && context->root->glob && funcform->provolatile == PROVOLATILE_STABLE)
 	{
 		 /* okay, but we cannot reuse this plan */
 		context->root->glob->oneoffPlan = true;

src/test/regress/expected/copy2.out
@@ -26,19 +26,19 @@ FOR EACH ROW EXECUTE PROCEDURE fn_x_before();
 COPY x (a, b, c, d, e) from stdin;
 ERROR:  function cannot execute on a QE slice because it issues a non-SELECT statement
 CONTEXT:  SQL statement "UPDATE x set e='after trigger fired' where c='stuff'"
-PL/pgSQL function "fn_x_after" line 3 at SQL statement
+PL/pgSQL function fn_x_after() line 3 at SQL statement
 COPY x (b, d) from stdin;
 ERROR:  function cannot execute on a QE slice because it issues a non-SELECT statement
 CONTEXT:  SQL statement "UPDATE x set e='after trigger fired' where c='stuff'"
-PL/pgSQL function "fn_x_after" line 3 at SQL statement
+PL/pgSQL function fn_x_after() line 3 at SQL statement
 COPY x (b, d) from stdin;
 ERROR:  function cannot execute on a QE slice because it issues a non-SELECT statement
 CONTEXT:  SQL statement "UPDATE x set e='after trigger fired' where c='stuff'"
-PL/pgSQL function "fn_x_after" line 3 at SQL statement
+PL/pgSQL function fn_x_after() line 3 at SQL statement
 COPY x (a, b, c, d, e) from stdin;
 ERROR:  function cannot execute on a QE slice because it issues a non-SELECT statement
 CONTEXT:  SQL statement "UPDATE x set e='after trigger fired' where c='stuff'"
-PL/pgSQL function "fn_x_after" line 3 at SQL statement
+PL/pgSQL function fn_x_after() line 3 at SQL statement
 -- non-existent column in column list: should fail
 COPY x (xyz) from stdin;
 ERROR:  column "xyz" of relation "x" does not exist
@@ -63,15 +63,15 @@ CONTEXT:  COPY x, line 1: "2002	232	40	50	60	70	80"
 COPY x (b, c, d, e) from stdin with oids delimiter ',' null 'x';
 ERROR:  function cannot execute on a QE slice because it issues a non-SELECT statement
 CONTEXT:  SQL statement "UPDATE x set e='after trigger fired' where c='stuff'"
-PL/pgSQL function "fn_x_after" line 3 at SQL statement
+PL/pgSQL function fn_x_after() line 3 at SQL statement
 COPY x from stdin WITH DELIMITER AS ';' NULL AS '';
 ERROR:  function cannot execute on a QE slice because it issues a non-SELECT statement
 CONTEXT:  SQL statement "UPDATE x set e='after trigger fired' where c='stuff'"
-PL/pgSQL function "fn_x_after" line 3 at SQL statement
+PL/pgSQL function fn_x_after() line 3 at SQL statement
 COPY x from stdin WITH DELIMITER AS ':' NULL AS E'\\X' ENCODING 'sql_ascii';
 ERROR:  function cannot execute on a QE slice because it issues a non-SELECT statement
 CONTEXT:  SQL statement "UPDATE x set e='after trigger fired' where c='stuff'"
-PL/pgSQL function "fn_x_after" line 3 at SQL statement
+PL/pgSQL function fn_x_after() line 3 at SQL statement
 -- check results of copy in
 SELECT * FROM x;
  a | b | c | d | e 