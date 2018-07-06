dd44c5dc933e6ccb2e00126b68e82c5502053e2a
Haozhou Wang <hawang@pivotal.io>
2018-07-06 16:39:16 +0800

    Merge plperl to PostgreSQL 9.2

--- src/pl/plperl/sql/plperl_init.sql
+++ src/pl/plperl/sql/plperl_init.sql
@@ -1,10 +1,6 @@
 -- test plperl.on_plperl_init errors are fatal
 
-<<<<<<< HEAD
--- Must load plperl before we can set on_plperl_init
-=======
 -- This test tests setting on_plperl_init after loading plperl
->>>>>>> 80edfd76591fdb9beec061de3c05ef4e9d96ce56
 LOAD 'plperl';
 
 SET SESSION plperl.on_plperl_init = ' system("/nonesuch"); ';
