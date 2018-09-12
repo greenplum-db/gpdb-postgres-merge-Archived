ea54b32a159c1d9011faa9e8a77efa42a8e35ab4
Heikki Linnakangas <hlinnakangas@pivotal.io>
2018-09-12 14:26:28 +0300

    Fix whitespace and a line that went missing during merge.

--- src/test/regress/sql/json.sql
+++ src/test/regress/sql/json.sql
@@ -155,14 +155,15 @@ SELECT test_json->'field2'
 FROM test_json
 WHERE json_type = 'object';
 
-SELECT test_json->>'field2' 
+SELECT test_json->>'field2'
 FROM test_json
 WHERE json_type = 'object';
 
-SELECT test_json -> 2 
+SELECT test_json -> 2
 FROM test_json
 WHERE json_type = 'scalar';
 
+SELECT test_json -> 2
 FROM test_json
 WHERE json_type = 'array';
 
