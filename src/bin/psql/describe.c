dcd1dc93cdd92e0edddcf93d7a1403cb2057f2da
Heikki Linnakangas <hlinnakangas@pivotal.io>
2018-09-25 16:35:16 +0300

    Fix psql query for \d.
    A new 9.4 copy of this query was added, but the 'relstorage' column was
    missing from it.

--- src/bin/psql/describe.c
+++ src/bin/psql/describe.c
@@ -1426,6 +1426,7 @@ describeOneTableDetails(const char *schemaname,
 						  "%s, c.reltablespace, "
 						  "CASE WHEN c.reloftype = 0 THEN '' ELSE c.reloftype::pg_catalog.regtype::pg_catalog.text END, "
 						  "c.relpersistence, c.relreplident\n"
+						  ", %s as relstorage "
 						  "FROM pg_catalog.pg_class c\n "
 		   "LEFT JOIN pg_catalog.pg_class tc ON (c.reltoastrelid = tc.oid)\n"
 						  "WHERE c.oid = '%s';",
@@ -1433,6 +1434,8 @@ describeOneTableDetails(const char *schemaname,
 						   "pg_catalog.array_to_string(c.reloptions || "
 						   "array(select 'toast.' || x from pg_catalog.unnest(tc.reloptions) x), ', ')\n"
 						   : "''"),
+						  /* GPDB Only:  relstorage  */
+						  (isGPDB() ? "c.relstorage" : "'h'"),
 						  oid);
 	}
 	else if (pset.sversion >= 90100)
