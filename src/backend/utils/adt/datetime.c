bba68ea94b2137a816afee1930a57231935fcaa4
Heikki Linnakangas <hlinnakangas@pivotal.io>
2018-09-27 11:28:31 +0300

    Fix timestamptz failure.
    I'm not sure why we have this extra code in GPDB, but it was causing
    the new parsing of years with 5 or more digits from upstream to fail.
    Added a FIXME to investigate.

--- src/backend/utils/adt/datetime.c
+++ src/backend/utils/adt/datetime.c
@@ -2724,8 +2724,29 @@ DecodeNumberField(int len, char *str, int fmask,
 		len = strlen(str);
 	}
 	/* No decimal point and no complete date yet? */
-	if ((fmask & DTK_DATE_M) != DTK_DATE_M && len <= 8 && !have_frac)
+	if ((fmask & DTK_DATE_M) != DTK_DATE_M && !have_frac)
 	{
+		/* GPDB_94_MERGE_FIXME: Why does GPDB have this special case for this case?
+		/* yyyymmddhhmmss? */
+		if ((fmask & DTK_DATE_M) != DTK_DATE_M && 
+			(fmask & DTK_TIME_M) != DTK_TIME_M &&
+			len == 14)
+		{
+            *tmask = DTK_DATE_M | DTK_TIME_M; 
+
+            tm->tm_sec = atoi(str + 12); 
+            *(str + 12) = '\0'; 
+            tm->tm_min = atoi(str + 10); 
+            *(str + 10) = '\0'; 
+            tm->tm_hour = atoi(str + 8); 
+            *(str + 8) = '\0'; 
+            tm->tm_mday = atoi(str + 6); 
+            *(str + 6) = '\0'; 
+            tm->tm_mon = atoi(str + 4); 
+            *(str + 4) = '\0'; 
+            tm->tm_year = atoi(str + 0); 
+            return DTK_DATE; 
+        } 
 		if (len >= 6)
 		{
 			*tmask = DTK_DATE_M;
@@ -2744,28 +2765,6 @@ DecodeNumberField(int len, char *str, int fmask,
 
 			return DTK_DATE;
 		}
-	}
-	else if ((fmask & DTK_DATE_M) != DTK_DATE_M && 
-			 (fmask & DTK_TIME_M) != DTK_TIME_M)
-	{
-        /* yyyymmddhhmmss? */ 
-        if (len == 14) 
-        { 
-            *tmask = DTK_DATE_M | DTK_TIME_M; 
-
-            tm->tm_sec = atoi(str + 12); 
-            *(str + 12) = '\0'; 
-            tm->tm_min = atoi(str + 10); 
-            *(str + 10) = '\0'; 
-            tm->tm_hour = atoi(str + 8); 
-            *(str + 8) = '\0'; 
-            tm->tm_mday = atoi(str + 6); 
-            *(str + 6) = '\0'; 
-            tm->tm_mon = atoi(str + 4); 
-            *(str + 4) = '\0'; 
-            tm->tm_year = atoi(str + 0); 
-            return DTK_DATE; 
-        } 
 	}
 
 	/* not all time fields are specified? */
@@ -4083,6 +4082,7 @@ AddVerboseIntPart(char *cp, int value, const char *units,
 	*is_zero = FALSE;
 	return cp + strlen(cp);
 }
+
 
 /* EncodeInterval()
  * Interpret time structure as a delta time and convert to string.
