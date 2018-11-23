ccf7ab1c5c80be91d9011e845bb616801daa886d
Paul Guo <paulguo@gmail.com>
2018-11-23 13:13:18 +0800

    Partially fix test rowtypes.
    Only one failure exists in rowtypes. Assume just a bit more effort is needed.
    Probably it will be an easy fix but I do not remember much about the GPDB CTE
    implemenation.

--- src/test/regress/sql/rowtypes.sql
+++ src/test/regress/sql/rowtypes.sql
@@ -272,6 +272,16 @@ select row_to_json(ss) from
 select row_to_json(ss) from
   (select q1 as a, q2 as b from int8_tbl offset 0) as ss(x,y);
 
+-- start_ignore
+-- GPDB: Make the plan same as PG's to make further merge and testing easy.
+-- PG plan is index only scan, while GP plan is seq scan without the guc
+-- settings below. That is because:
+-- 1) Default large (different as PG's) random_page_cost makes planner incline to seq scan.
+-- 2) Low vis fraction estimation (pg_class.relallvisible) on GP causes higher
+--    index only scan cost so bitmap index scan outperforms index only scan.
+-- end_ignore
+set random_page_cost = 4;
+set enable_bitmapscan = off;
 explain (costs off)
 select row_to_json(q) from
   (select thousand, tenthous from tenk1
@@ -285,8 +295,13 @@ select row_to_json(q) from
 select row_to_json(q) from
   (select thousand as x, tenthous as y from tenk1
    where thousand = 42 and tenthous < 2000 offset 0) q(a,b);
+-- start_ignore
+-- restore to the default or previously set values.
+reset random_page_cost;
+reset enable_bitmapscan;
+-- end_ignore
 
-create temp table tt1 as select * from int8_tbl limit 2;
+create temp table tt1 as select * from int8_tbl order by 1 limit 2;
 create temp table tt2 () inherits(tt1);
 insert into tt2 values(0,0);
 select row_to_json(r) from (select q2,q1 from tt1 offset 0) r;
