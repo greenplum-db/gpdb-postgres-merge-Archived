2cea5cb5a0ac83373f6aa966b3a0e60f9abb997f
Paul Guo <paulguo@gmail.com>
2018-12-05 17:19:32 +0800

    Force gpdiff to sort the query result for a query that includes order by in subquery.
    For that kind of query, gpdiff will not sort the the query result.

--- src/test/regress/sql/join.sql
+++ src/test/regress/sql/join.sql
@@ -417,7 +417,7 @@ select * from
 select * from
   j1_tbl full join
   (select * from j2_tbl order by j2_tbl.i desc, j2_tbl.k asc) j2_tbl
-  on j1_tbl.i = j2_tbl.i and j1_tbl.i = j2_tbl.k;
+  on j1_tbl.i = j2_tbl.i and j1_tbl.i = j2_tbl.k; --order none
 reset enable_mergejoin;
 reset enable_hashjoin;
 
