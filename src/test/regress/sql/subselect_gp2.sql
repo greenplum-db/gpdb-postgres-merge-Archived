859384e8129e24937f93c6db9b18c6f58fee6f63
Paul Guo <paulguo@gmail.com>
2018-12-21 17:42:56 +0800

    Add cost info in explain in test subselect_gp2
    This helps debugging.
    
    I saw it failed in icw_planner_centos7_online_expand.

--- src/test/regress/sql/subselect_gp2.sql
+++ src/test/regress/sql/subselect_gp2.sql
@@ -22,7 +22,7 @@ select * from test_ext_foo as o
 where (select count(*) from echotable as i where i.c2 = o.c2) >= 2;
 
 -- Planner test to make sure the initplan is not removed for function scan
-explain (costs off)
+explain
 select sess_id from pg_stat_activity
 where query = (select current_query())
 and usename='xxx' and datname='xxx';
