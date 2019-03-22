a6067883d62c1c5591f6ac75be635ad5f280a640
Georgios Kokolatos <gkokolatos@pivotal.io>
2019-03-22 08:47:31 +0000

    Resove merge conflicts in src/backend/commands/opclasscmds.c

--- src/backend/commands/opclasscmds.c
+++ src/backend/commands/opclasscmds.c
@@ -25,11 +25,8 @@
 #include "catalog/dependency.h"
 #include "catalog/indexing.h"
 #include "catalog/objectaccess.h"
-<<<<<<< HEAD
 #include "catalog/oid_dispatch.h"
-=======
 #include "catalog/opfam_internal.h"
->>>>>>> ab93f90cd3a4fcdd891cee9478941c3cc65795b8
 #include "catalog/pg_amop.h"
 #include "catalog/pg_amproc.h"
 #include "catalog/pg_namespace.h"
