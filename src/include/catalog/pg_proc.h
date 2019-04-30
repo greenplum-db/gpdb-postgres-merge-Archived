77773be6efe91a9e3f8ddfc110cfd7f4439e2b54
Jimmy Yih <jyih@pivotal.io>
2019-04-30 16:16:59 -0700

    Fix pg_event_trigger_dropped_objects pg_proc catalog entry
    This was not resolved correctly during the merge conflict phase. The
    event_trigger regress test was failing to create a memtuple with tuple
    descriptor obtained from pg_proc but the pg_proc entry was incorrect.

--- src/include/catalog/pg_proc.h
+++ src/include/catalog/pg_proc.h
@@ -5317,7 +5317,7 @@ DESCR("peek at binary changes from replication slot");
 
 /* event triggers */
 GPDB_EXTRA_COL(proexeclocation = m);
-DATA(insert OID = 3566 (  pg_event_trigger_dropped_objects		PGNSP PGUID 12 10 100 0 0 f f f f t t s 0 0 2249 "" "{26,26,23,25,25,25,25}" "{o,o,o,o,o,o,o}" "{classid, objid, objsubid, object_type, schema_name, object_name, object_identity}" _null_ _null_ pg_event_trigger_dropped_objects _null_ _null_ _null_ ));
+DATA(insert OID = 3566 (  pg_event_trigger_dropped_objects		PGNSP PGUID 12 10 100 0 0 f f f f t t s 0 0 2249 "" "{26,26,23,16,16,16,25,25,25,25,1009,1009}" "{o,o,o,o,o,o,o,o,o,o,o,o}" "{classid, objid, objsubid, original, normal, is_temporary, object_type, schema_name, object_name, object_identity, address_names, address_args}" _null_ _null_ pg_event_trigger_dropped_objects _null_ _null_ _null_ ));
 DESCR("list objects dropped by the current command");
 DATA(insert OID = 4566 (  pg_event_trigger_table_rewrite_oid	PGNSP PGUID 12 1 0 0 0 f f f f t f s 0 0 26 "" "{26}" "{o}" "{oid}" _null_ _null_ pg_event_trigger_table_rewrite_oid _null_ _null_ _null_ ));
 DESCR("return Oid of the table getting rewritten");
