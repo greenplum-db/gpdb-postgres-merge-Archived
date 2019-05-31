64681f1c0eec506aad3fd07f4e51d26604369eaf
Chris Hajas <chajas@pivotal.io>
2019-05-31 16:42:06 -0700

    Update ORCA answer files
    brin, qp_misc_jiras, with, and partially gporca
    
    - ORCA does not currently support BRIN indexes
    
    - ORCA ctas creates tables with a random distribution policy by default
    (compared to using the first column with planner). This is also the case in 5X and 6X.
    Therefore, explicitly specify the distribution policy to prevent
    subsequent errors due to a random distribution policy.
    
    Authored-by: Chris Hajas <chajas@pivotal.io>

--- src/test/regress/sql/with.sql
+++ src/test/regress/sql/with.sql
@@ -861,7 +861,7 @@ SELECT a BETWEEN 0 AND 4200 FROM t LIMIT 10;
 SELECT * FROM y;
 
 -- data-modifying WITH containing INSERT...ON CONFLICT DO UPDATE
-CREATE TABLE z AS SELECT i AS k, (i || ' v')::text v FROM generate_series(1, 16, 3) i;
+CREATE TABLE z AS SELECT i AS k, (i || ' v')::text v FROM generate_series(1, 16, 3) i DISTRIBUTED BY (k);
 ALTER TABLE z ADD UNIQUE (k);
 
 WITH t AS (
