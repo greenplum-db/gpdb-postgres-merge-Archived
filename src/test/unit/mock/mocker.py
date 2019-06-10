41e9c78b214fe4613cce2e11251802f1f96dcda8
Georgios Kokolatos <gkokolatos@pivotal.io>
2019-06-10 15:48:25 +0000

    Allow mocker to parse long long type

--- src/test/unit/mock/mocker.py
+++ src/test/unit/mock/mocker.py
@@ -165,7 +165,7 @@ class FuncSignature(object):
     # we need extra space at the end.
     arg_pat = re.compile(
         # argtype.  i.e. 'const unsigned long', 'struct Foo *', 'const char * const'
-        r'((?:register\s+|const\s+|volatile\s+)*(?:enum\s+|struct\s+|unsigned\s+|long\s+)?' +
+        r'((?:register\s+|const\s+|volatile\s+)*(?:enum\s+|struct\s+|unsigned\s+|long\s+long\s+|long\s+)?' +
             r'\w+(?:[\s\*]+)(?:const[\s\*]+)?|\s+)' +
         r'(?:__restrict\s+)?' +
         # argname.  We accept 'arg[]'
