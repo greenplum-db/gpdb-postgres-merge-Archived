a459d632e58a3c6d1029187c1327ffc264a09d17
Georgios Kokolatos <gkokolatos@pivotal.io>
2019-09-03 22:40:41 +0000

    Update conflict resolution in lock.h
    	Follow the change of the upstream commit 4eda0a647

--- src/include/storage/lock.h
+++ src/include/storage/lock.h
@@ -131,27 +131,6 @@ typedef uint16 LOCKMETHODID;
 #define RESOURCE_LOCKMETHOD	3
 
 /*
- * These are the valid values of type LOCKMODE for all the standard lock
- * methods (both DEFAULT and USER).
- */
-
-/* NoLock is not a lock mode, but a flag value meaning "don't get a lock" */
-#define NoLock					0
-
-#define AccessShareLock			1		/* SELECT */
-#define RowShareLock			2		/* SELECT FOR UPDATE/FOR SHARE */
-#define RowExclusiveLock		3		/* INSERT, UPDATE, DELETE */
-#define ShareUpdateExclusiveLock 4		/* VACUUM (non-FULL),ANALYZE, CREATE
-										 * INDEX CONCURRENTLY */
-#define ShareLock				5		/* CREATE INDEX (WITHOUT CONCURRENTLY) */
-#define ShareRowExclusiveLock	6		/* like EXCLUSIVE MODE, but allows ROW
-										 * SHARE */
-#define ExclusiveLock			7		/* blocks ROW SHARE/SELECT...FOR
-										 * UPDATE */
-#define AccessExclusiveLock		8		/* ALTER TABLE, DROP TABLE, VACUUM
-										 * FULL, and unqualified LOCK TABLE */
-
-/*
  * Map from lock methods to lock table data structures.
  */
 extern const	LockMethodData resource_lockmethod;
