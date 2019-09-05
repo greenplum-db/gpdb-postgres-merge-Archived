ce812fd543ebcce3450323a9ec40617a65195e47
Weinan Wang <wewang@pivotal.io>
2019-09-05 14:58:43 +0800

    Resolve conflict in execScan.c

--- src/backend/executor/execScan.c
+++ src/backend/executor/execScan.c
@@ -7,13 +7,9 @@
  *	  stuff - checking the qualification and projecting the tuple
  *	  appropriately.
  *
-<<<<<<< HEAD
  * Portions Copyright (c) 2006 - present, EMC/Greenplum
  * Portions Copyright (c) 2012-Present Pivotal Software, Inc.
- * Portions Copyright (c) 1996-2015, PostgreSQL Global Development Group
-=======
  * Portions Copyright (c) 1996-2016, PostgreSQL Global Development Group
->>>>>>> b5bce6c1ec6061c8a4f730d927e162db7e2ce365
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  *
