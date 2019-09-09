5b48a4d157834828196941bdd15df1983784a37a
Georgios Kokolatos <gkokolatos@pivotal.io>
2019-09-09 16:40:22 +0200

    Resolve conflicts in dumputils.c maintaining backports.
    There exist a few GPDB only functions that should be moved in the new string
    utils file, though this is left as a post merge work. Same goes for backports
    that were introduced after string utils. We will catch up with them and remove
    them then from dumputils.

--- src/bin/pg_dump/dumputils.h
+++ src/bin/pg_dump/dumputils.h
@@ -36,60 +36,27 @@
 #endif
 
 
-<<<<<<< HEAD
-extern const char *fmtId(const char *identifier);
-extern const char *fmtQualifiedId(int remoteVersion,
-			   const char *schema, const char *id);
 extern char *formatPGVersionNumber(int version_number, bool include_minor,
 					  char *buf, size_t buflen);
-extern void appendStringLiteral(PQExpBuffer buf, const char *str,
-					int encoding, bool std_strings);
-extern void appendStringLiteralConn(PQExpBuffer buf, const char *str,
-						PGconn *conn);
-extern void appendStringLiteralDQ(PQExpBuffer buf, const char *str,
-					  const char *dqprefix);
-extern void appendByteaLiteral(PQExpBuffer buf,
-				   const unsigned char *str, size_t length,
-				   bool std_strings);
-extern void appendShellString(PQExpBuffer buf, const char *str);
-extern void appendConnStrVal(PQExpBuffer buf, const char *str);
-extern void appendPsqlMetaConnect(PQExpBuffer buf, const char *dbname);
-extern bool parsePGArray(const char *atext, char ***itemarray, int *nitems);
 extern bool buildACLCommands(const char *name, const char *subname, const char *nspname,
-				 const char *type, const char *acls, const char *owner,
-				 const char *prefix, int remoteVersion,
-=======
-extern bool buildACLCommands(const char *name, const char *subname,
 				 const char *type, const char *acls, const char *racls,
 				 const char *owner, const char *prefix, int remoteVersion,
->>>>>>> b5bce6c1ec6061c8a4f730d927e162db7e2ce365
 				 PQExpBuffer sql);
 extern bool buildDefaultACLCommands(const char *type, const char *nspname,
 						const char *acls, const char *owner,
 						int remoteVersion,
 						PQExpBuffer sql);
-<<<<<<< HEAD
-extern bool processSQLNamePattern(PGconn *conn, PQExpBuffer buf,
-					  const char *pattern,
-					  bool have_where, bool force_escape,
-					  const char *schemavar, const char *namevar,
-					  const char *altnamevar, const char *visibilityrule);
 /* GPDB additions */
 extern char *escape_backslashes(const char *src, bool quotes_too);
 extern char *escape_fmtopts_string(const char *src);
 extern char *custom_fmtopts_string(const char *src);
 
-=======
->>>>>>> b5bce6c1ec6061c8a4f730d927e162db7e2ce365
 extern void buildShSecLabelQuery(PGconn *conn, const char *catalog_name,
 					 Oid objectId, PQExpBuffer sql);
 extern void emitShSecLabels(PGconn *conn, PGresult *res,
-<<<<<<< HEAD
 				PQExpBuffer buffer, const char *objtype, const char *objname);
+
 extern void set_dump_section(const char *arg, int *dumpSections);
-=======
-				PQExpBuffer buffer, const char *target, const char *objname);
->>>>>>> b5bce6c1ec6061c8a4f730d927e162db7e2ce365
 
 extern void buildACLQueries(PQExpBuffer acl_subquery, PQExpBuffer racl_subquery,
 				PQExpBuffer init_acl_subquery, PQExpBuffer init_racl_subquery,
