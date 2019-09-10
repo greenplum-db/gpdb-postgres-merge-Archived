dcd50ba1d2418cf903a49dad6c58977bc836fe47
WANG Weinan <wewang@pivotal.io>
2019-09-10 16:34:16 +0800

    Resolve conflict in 001_pg_controldata.pl

--- src/bin/pg_controldata/t/001_pg_controldata.pl
+++ src/bin/pg_controldata/t/001_pg_controldata.pl
@@ -10,14 +10,10 @@ program_options_handling_ok('pg_controldata');
 command_fails(['pg_controldata'], 'pg_controldata without arguments fails');
 command_fails([ 'pg_controldata', 'nonexistent' ],
 	'pg_controldata with nonexistent directory fails');
-<<<<<<< HEAD
-system_or_bail 'initdb', '-D', "$tempdir/data", '-A', 'trust';
-command_like([ 'pg_controldata', "$tempdir/data" ],
-=======
 
 my $node = get_new_node('main');
 $node->init;
 
+system_or_bail 'initdb', '-D', $node->data_dir, '-A', 'trust';
 command_like([ 'pg_controldata', $node->data_dir ],
->>>>>>> b5bce6c1ec6061c8a4f730d927e162db7e2ce365
 	qr/checkpoint/, 'pg_controldata produces output');
