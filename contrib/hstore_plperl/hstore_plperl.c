c9e371aab5abff372a0773f7fa5da3f88e413430
Heikki Linnakangas <hlinnakangas@pivotal.io>
2019-10-08 10:35:04 +0300

    Make 'hstore_plperl' compile.
    By replacing hstore_plperl.c with the latest version from upstream
    REL9_6_STABLE. There were some portability fixes there.

--- contrib/hstore_plperl/hstore_plperl.c
+++ contrib/hstore_plperl/hstore_plperl.c
@@ -13,6 +13,7 @@ PG_FUNCTION_INFO_V1(hstore_to_plperl);
 Datum
 hstore_to_plperl(PG_FUNCTION_ARGS)
 {
+	dTHX;
 	HStore	   *in = PG_GETARG_HS(0);
 	int			i;
 	int			count = HS_COUNT(in);
@@ -45,6 +46,8 @@ PG_FUNCTION_INFO_V1(plperl_to_hstore);
 Datum
 plperl_to_hstore(PG_FUNCTION_ARGS)
 {
+	dTHX;
+	SV		   *in = (SV *) PG_GETARG_POINTER(0);
 	HV		   *hv;
 	HE		   *he;
 	int32		buflen;
@@ -53,7 +56,16 @@ plperl_to_hstore(PG_FUNCTION_ARGS)
 	HStore	   *out;
 	Pairs	   *pairs;
 
-	hv = (HV *) SvRV((SV *) PG_GETARG_POINTER(0));
+	/* Dereference references recursively. */
+	while (SvROK(in))
+		in = SvRV(in);
+
+	/* Now we must have a hash. */
+	if (SvTYPE(in) != SVt_PVHV)
+		ereport(ERROR,
+				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+				 (errmsg("cannot transform non-hash Perl value to hstore"))));
+	hv = (HV *) in;
 
 	pcount = hv_iterinit(hv);
 
