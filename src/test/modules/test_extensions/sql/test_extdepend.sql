df0bd71f6c3bebb97dce868b82e0e712794df8bc
Melanie Plageman <mplageman@pivotal.io>
2019-10-09 14:09:07 -0700

    Partially fix/prep test_extdepend.sql
    This enforces the order of commands executed out of
    test_extdep_commands. Fixing it here since this test file is newly
    introduced in 9.6
    
    Co-authored-by: Soumyadeep Chakraborty <sochakraborty@pivotal.io>

--- src/test/modules/test_extensions/sql/test_extdepend.sql
+++ src/test/modules/test_extensions/sql/test_extdepend.sql
@@ -3,30 +3,31 @@
 --
 
 -- Common setup for all tests
-CREATE TABLE test_extdep_commands (command text);
-COPY test_extdep_commands FROM stdin;
- CREATE SCHEMA test_ext
- CREATE EXTENSION test_ext5 SCHEMA test_ext
- SET search_path TO test_ext
- CREATE TABLE a (a1 int)
-
- CREATE FUNCTION b() RETURNS TRIGGER LANGUAGE plpgsql AS\n   $$ BEGIN NEW.a1 := NEW.a1 + 42; RETURN NEW; END; $$
- ALTER FUNCTION b() DEPENDS ON EXTENSION test_ext5
-
- CREATE TRIGGER c BEFORE INSERT ON a FOR EACH ROW EXECUTE PROCEDURE b()
- ALTER TRIGGER c ON a DEPENDS ON EXTENSION test_ext5
-
- CREATE MATERIALIZED VIEW d AS SELECT * FROM a
- ALTER MATERIALIZED VIEW d DEPENDS ON EXTENSION test_ext5
-
- CREATE INDEX e ON a (a1)
- ALTER INDEX e DEPENDS ON EXTENSION test_ext5
- RESET search_path
+-- GPDB: We have added a serial number column (i) since the order of execution
+-- of these commands matter due to inter-command dependencies.
+CREATE TABLE test_extdep_commands (i int, command text);
+COPY test_extdep_commands FROM stdin WITH DELIMITER ',';
+ 1,CREATE SCHEMA test_ext
+ 2,CREATE EXTENSION test_ext5 SCHEMA test_ext
+ 3,SET search_path TO test_ext
+ 4,CREATE TABLE a (a1 int)
+ 5,
+ 6,CREATE FUNCTION b() RETURNS TRIGGER LANGUAGE plpgsql AS\n   $$ BEGIN NEW.a1 := NEW.a1 + 42; RETURN NEW; END; $$
+ 7,ALTER FUNCTION b() DEPENDS ON EXTENSION test_ext5
+ 8,
+ 9,CREATE TRIGGER c BEFORE INSERT ON a FOR EACH ROW EXECUTE PROCEDURE b()
+ 10,ALTER TRIGGER c ON a DEPENDS ON EXTENSION test_ext5
+ 11,
+ 12,CREATE MATERIALIZED VIEW d AS SELECT * FROM a
+ 13,ALTER MATERIALIZED VIEW d DEPENDS ON EXTENSION test_ext5
+ 14,
+ 15,CREATE INDEX e ON a (a1)
+ 16,ALTER INDEX e DEPENDS ON EXTENSION test_ext5
+ 17,RESET search_path
 \.
-
-SELECT * FROM test_extdep_commands;
+SELECT command FROM test_extdep_commands ORDER BY i;
 -- First, test that dependent objects go away when the extension is dropped.
-SELECT * FROM test_extdep_commands \gexec
+SELECT command FROM test_extdep_commands ORDER BY i \gexec
 -- make sure we have the right dependencies on the extension
 SELECT deptype, p.*
   FROM pg_depend, pg_identify_object(classid, objid, objsubid) AS p
@@ -43,7 +44,7 @@ DROP SCHEMA test_ext CASCADE;
 
 -- Second test: If we drop the table, the objects are dropped too and no
 -- vestige remains in pg_depend.
-SELECT * FROM test_extdep_commands \gexec
+SELECT command FROM test_extdep_commands ORDER BY i \gexec
 DROP TABLE test_ext.a;		-- should fail, require cascade
 DROP TABLE test_ext.a CASCADE;
 -- anything still depending on the extension?  Should be only function b()
@@ -55,7 +56,7 @@ DROP EXTENSION test_ext5;
 DROP SCHEMA test_ext CASCADE;
 
 -- Third test: we can drop the objects individually
-SELECT * FROM test_extdep_commands \gexec
+SELECT command FROM test_extdep_commands ORDER BY i \gexec
 SET search_path TO test_ext;
 DROP TRIGGER c ON a;
 DROP FUNCTION b();
