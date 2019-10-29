57afc6096dcee3303a791fdb78f2cca4e17a3930
Heikki Linnakangas <hlinnakangas@pivotal.io>
2019-10-29 18:21:32 +0200

    On second thoughs, better to not try to "justify" the resgroup wait time.
    Looking closer at the old implementation, I believe it didn't justify
    either. And the user can call 'justify_interval' if that's what she wants.
    
    The reason I'm second-guessing commit f9881030a6 is that it didn't compile:
    
    resgroup.c: In function ‘ResGroupGetStat’:
    resgroup.c:1024:73: error: ‘MSECS_PER_SECOND’ undeclared (first use in this function); did you mean ‘USECS_PER_SEC’?
        interval->month = ms_left / ((int64) DAYS_PER_MONTH * SECS_PER_DAY * MSECS_PER_SECOND);
                                                                             ^~~~~~~~~~~~~~~~
                                                                             USECS_PER_SEC

--- src/backend/utils/resgroup/resgroup.c
+++ src/backend/utils/resgroup/resgroup.c
@@ -994,7 +994,6 @@ ResGroupGetStat(Oid groupId, ResGroupStatType type)
 	ResGroupData *group;
 	Interval   *interval;
 	Datum		result;
-	int64		ms_left;
 
 	Assert(IsResGroupActivated());
 
@@ -1017,18 +1016,16 @@ ResGroupGetStat(Oid groupId, ResGroupStatType type)
 			result = Int32GetDatum(group->totalQueued);
 			break;
 		case RES_GROUP_STAT_TOTAL_QUEUE_TIME:
-			/* turn milliseconds in totalQueuedTimeMs into an Interval. */
+			/*
+			 * Turn milliseconds in totalQueuedTimeMs into an Interval.
+			 *
+			 * Note: we only use the 'time' field. The user can call
+			 * justify_interval() if she wants.
+			 */
 			interval = (Interval *) palloc(sizeof(Interval));
-			ms_left = group->totalQueuedTimeMs;
-
-			interval->month = ms_left / ((int64) DAYS_PER_MONTH * SECS_PER_DAY * MSECS_PER_SECOND);
-			ms_left -= (int64) DAYS_PER_MONTH * SECS_PER_DAY * MSECS_PER_SECOND * interval->month;
-
-			interval->day = ms_left / ((int64) SECS_PER_DAY * MSECS_PER_SECOND);
-			ms_left -= (int64) SECS_PER_DAY * MSECS_PER_SECOND * interval->day;
-
-			interval->time = ms_left * 1000;
-
+			interval->time = group->totalQueuedTimeMs * 1000;
+			interval->day = 0;
+			interval->month = 0;
 			result = IntervalPGetDatum(interval);
 			break;
 		case RES_GROUP_STAT_MEM_USAGE:
