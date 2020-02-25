e84b6e6036cbff7f53a8b72f2b1047938f1910bf
Heikki Linnakangas <hlinnakangas@pivotal.io>
2020-02-25 10:24:07 +0200

    More fixes to the way libpq functions are linked into the backend.
    Disable the "link canary" check in backend.

--- src/interfaces/libpq/fe-connect.c
+++ src/interfaces/libpq/fe-connect.c
@@ -1952,12 +1952,14 @@ connectDBStart(PGconn *conn)
 	 * Nobody but developers should see this message, so we don't bother
 	 * translating it.
 	 */
+#ifdef FRONTEND
 	if (!pg_link_canary_is_frontend())
 	{
 		printfPQExpBuffer(&conn->errorMessage,
 						  "libpq is incorrectly linked to backend functions\n");
 		goto connect_errReturn;
 	}
+#endif
 
 	/* Ensure our buffers are empty */
 	conn->inStart = conn->inCursor = conn->inEnd = 0;
