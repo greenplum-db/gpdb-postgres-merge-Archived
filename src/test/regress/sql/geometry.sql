ba906b713813207819b0a13b8a5c46b82de371ee
Heikki Linnakangas <hlinnakangas@pivotal.io>
2020-02-27 18:18:34 +0200

    Revert geometry test to upstream version.
    I don't see any valid reason to differ from upstream. It's not passing
    yet, but I think what we need to do is to fix the code so that it passes.

--- src/test/regress/sql/geometry.sql
+++ src/test/regress/sql/geometry.sql
@@ -2,52 +2,6 @@
 -- GEOMETRY
 --
 
--- create needed tables
-CREATE TABLE BOX_GEOM_TBL (f1 box);
-INSERT INTO BOX_GEOM_TBL (f1) VALUES ('(2.0,2.0,0.0,0.0)');
-INSERT INTO BOX_GEOM_TBL (f1) VALUES ('(1.0,1.0,3.0,3.0)');
-INSERT INTO BOX_GEOM_TBL (f1) VALUES ('(2.5, 2.5, 2.5,3.5)');
-INSERT INTO BOX_GEOM_TBL (f1) VALUES ('(3.0, 3.0,3.0,3.0)');
-
-CREATE TABLE CIRCLE_GEOM_TBL (f1 circle);
-INSERT INTO CIRCLE_GEOM_TBL VALUES ('<(5,1),3>');
-INSERT INTO CIRCLE_GEOM_TBL VALUES ('<(1,2),100>');
-INSERT INTO CIRCLE_GEOM_TBL VALUES ('1,3,5');
-INSERT INTO CIRCLE_GEOM_TBL VALUES ('((1,2),3)');
-INSERT INTO CIRCLE_GEOM_TBL VALUES ('<(100,200),10>');
-INSERT INTO CIRCLE_GEOM_TBL VALUES ('<(100,1),115>');
-
-CREATE TABLE LSEG_GEOM_TBL (s lseg);
-INSERT INTO LSEG_GEOM_TBL VALUES ('[(1,2),(3,4)]');
-INSERT INTO LSEG_GEOM_TBL VALUES ('(0,0),(6,6)');
-INSERT INTO LSEG_GEOM_TBL VALUES ('10,-10 ,-3,-4');
-INSERT INTO LSEG_GEOM_TBL VALUES ('[-1e6,2e2,3e5, -4e1]');
-INSERT INTO LSEG_GEOM_TBL VALUES ('(11,22,33,44)');
-
-CREATE TABLE PATH_GEOM_TBL (s serial, f1 path);
-INSERT INTO PATH_GEOM_TBL(f1) VALUES ('[(1,2),(3,4)]');
-INSERT INTO PATH_GEOM_TBL(f1) VALUES ('((1,2),(3,4))');
-INSERT INTO PATH_GEOM_TBL(f1) VALUES ('[(0,0),(3,0),(4,5),(1,6)]');
-INSERT INTO PATH_GEOM_TBL(f1) VALUES ('((1,2),(3,4))');
-INSERT INTO PATH_GEOM_TBL(f1) VALUES ('1,2 ,3,4');
-INSERT INTO PATH_GEOM_TBL(f1) VALUES ('[1,2,3, 4]');
-INSERT INTO PATH_GEOM_TBL(f1) VALUES ('[11,12,13,14]');
-INSERT INTO PATH_GEOM_TBL(f1) VALUES ('(11,12,13,14)');
-
-CREATE TABLE POLYGON_GEOM_TBL(s serial, f1 polygon);
-INSERT INTO POLYGON_GEOM_TBL(f1) VALUES ('(2.0,0.0),(2.0,4.0),(0.0,0.0)');
-INSERT INTO POLYGON_GEOM_TBL(f1) VALUES ('(3.0,1.0),(3.0,3.0),(1.0,0.0)');
-INSERT INTO POLYGON_GEOM_TBL(f1) VALUES ('(0.0,0.0)');
-INSERT INTO POLYGON_GEOM_TBL(f1) VALUES ('(0.0,1.0),(0.0,1.0)');
-
-CREATE TABLE POINT_GEOM_TBL(f1 point);
-INSERT INTO POINT_GEOM_TBL(f1) VALUES ('(0.0,0.0)');
-INSERT INTO POINT_GEOM_TBL(f1) VALUES ('(-10.0,0.0)');
-INSERT INTO POINT_GEOM_TBL(f1) VALUES ('(-3.0,4.0)');
-INSERT INTO POINT_GEOM_TBL(f1) VALUES ('(5.1, 34.5)');
-INSERT INTO POINT_GEOM_TBL(f1) VALUES ('(-5.0,-12.0)');
-INSERT INTO POINT_GEOM_TBL(f1) VALUES ('10.0,10.0');
-
 -- Back off displayed precision a little bit to reduce platform-to-platform
 -- variation in results.
 SET extra_float_digits TO -3;
@@ -57,39 +11,39 @@ SET extra_float_digits TO -3;
 --
 
 SELECT '' AS four, center(f1) AS center
-   FROM BOX_GEOM_TBL;
+   FROM BOX_TBL;
 
 SELECT '' AS four, (@@ f1) AS center
-   FROM BOX_GEOM_TBL;
+   FROM BOX_TBL;
 
 SELECT '' AS six, point(f1) AS center
-   FROM CIRCLE_GEOM_TBL;
+   FROM CIRCLE_TBL;
 
 SELECT '' AS six, (@@ f1) AS center
-   FROM CIRCLE_GEOM_TBL;
+   FROM CIRCLE_TBL;
 
 SELECT '' AS two, (@@ f1) AS center
-   FROM POLYGON_GEOM_TBL
+   FROM POLYGON_TBL
    WHERE (# f1) > 2;
 
 -- "is horizontal" function
 SELECT '' AS two, p1.f1
-   FROM POINT_GEOM_TBL p1
+   FROM POINT_TBL p1
    WHERE ishorizontal(p1.f1, point '(0,0)');
 
 -- "is horizontal" operator
 SELECT '' AS two, p1.f1
-   FROM POINT_GEOM_TBL p1
+   FROM POINT_TBL p1
    WHERE p1.f1 ?- point '(0,0)';
 
 -- "is vertical" function
 SELECT '' AS one, p1.f1
-   FROM POINT_GEOM_TBL p1
+   FROM POINT_TBL p1
    WHERE isvertical(p1.f1, point '(5.1,34.5)');
 
 -- "is vertical" operator
 SELECT '' AS one, p1.f1
-   FROM POINT_GEOM_TBL p1
+   FROM POINT_TBL p1
    WHERE p1.f1 ?| point '(5.1,34.5)';
 
 -- Slope
@@ -195,13 +149,8 @@ SELECT l.s, b.f1, l.s ## b.f1 FROM LINE_TBL l, BOX_TBL b;
 
 -- intersection
 SELECT '' AS count, p.f1, l.s, l.s # p.f1 AS intersection
-   FROM LSEG_GEOM_TBL l, POINT_GEOM_TBL p;
+   FROM LSEG_TBL l, POINT_TBL p;
 
-<<<<<<< HEAD
--- closest point
-SELECT '' AS thirty, p.f1, l.s, p.f1 ## l.s AS closest
-   FROM LSEG_GEOM_TBL l, POINT_GEOM_TBL p;
-=======
 -- Length
 SELECT s, @-@ s FROM LSEG_TBL;
 
@@ -273,36 +222,25 @@ SELECT l.s, l1.s FROM LSEG_TBL l, LINE_TBL l1 WHERE l.s <@ l1.s;
 
 -- On box
 SELECT l.s, b.f1 FROM LSEG_TBL l, BOX_TBL b WHERE l.s <@ b.f1;
->>>>>>> 9e1c9f959422192bbe1b842a2a1ffaf76b080196
 
 --
 -- Boxes
 --
 
-SELECT '' as six, box(f1) AS box FROM CIRCLE_GEOM_TBL;
+SELECT '' as six, box(f1) AS box FROM CIRCLE_TBL;
 
 -- translation
 SELECT '' AS twentyfour, b.f1 + p.f1 AS translation
-   FROM BOX_GEOM_TBL b, POINT_GEOM_TBL p;
+   FROM BOX_TBL b, POINT_TBL p;
 
 SELECT '' AS twentyfour, b.f1 - p.f1 AS translation
-   FROM BOX_GEOM_TBL b, POINT_GEOM_TBL p;
+   FROM BOX_TBL b, POINT_TBL p;
 
-<<<<<<< HEAD
--- scaling and rotation
-SELECT '' AS twentyfour, b.f1 * p.f1 AS rotation
-   FROM BOX_GEOM_TBL b, POINT_GEOM_TBL p;
-
-SELECT '' AS twenty, b.f1 / p.f1 AS rotation
-   FROM BOX_GEOM_TBL b, POINT_GEOM_TBL p
-   WHERE (p.f1 <-> point '(0,0)') >= 1;
-=======
 -- Multiply with point
 SELECT b.f1, p.f1, b.f1 * p.f1 FROM BOX_TBL b, POINT_TBL p WHERE p.f1[0] BETWEEN 1 AND 1000;
 
 -- Overflow error
 SELECT b.f1, p.f1, b.f1 * p.f1 FROM BOX_TBL b, POINT_TBL p WHERE p.f1[0] > 1000;
->>>>>>> 9e1c9f959422192bbe1b842a2a1ffaf76b080196
 
 -- Divide by point
 SELECT b.f1, p.f1, b.f1 / p.f1 FROM BOX_TBL b, POINT_TBL p WHERE p.f1[0] BETWEEN 1 AND 1000;
@@ -333,19 +271,6 @@ SELECT b1.f1, b2.f1, b1.f1 <-> b2.f1 FROM BOX_TBL b1, BOX_TBL b2;
 -- Paths
 --
 
-<<<<<<< HEAD
-SELECT '' AS eight, npoints(f1) AS npoints, f1 AS path FROM PATH_GEOM_TBL;
-
-SELECT '' AS four, path(f1) FROM POLYGON_GEOM_TBL;
-
--- translation
-SELECT '' AS eight, p1.f1 + point '(10,10)' AS dist_add
-   FROM PATH_GEOM_TBL p1;
-
--- scaling and rotation
-SELECT '' AS eight, p1.f1 * point '(2,-1)' AS dist_mul
-   FROM PATH_GEOM_TBL p1;
-=======
 -- Points
 SELECT f1, npoints(f1) FROM PATH_TBL;
 
@@ -399,7 +324,6 @@ SELECT p.f1, p1.f1, p.f1 / p1.f1 FROM PATH_TBL p, POINT_TBL p1 WHERE p1.f1 ~= '(
 
 -- Distance to path
 SELECT p1.f1, p2.f1, p1.f1 <-> p2.f1 FROM PATH_TBL p1, PATH_TBL p2;
->>>>>>> 9e1c9f959422192bbe1b842a2a1ffaf76b080196
 
 --
 -- Polygons
@@ -407,33 +331,24 @@ SELECT p1.f1, p2.f1, p1.f1 <-> p2.f1 FROM PATH_TBL p1, PATH_TBL p2;
 
 -- containment
 SELECT '' AS twentyfour, p.f1, poly.f1, poly.f1 @> p.f1 AS contains
-   FROM POLYGON_GEOM_TBL poly, POINT_GEOM_TBL p;
+   FROM POLYGON_TBL poly, POINT_TBL p;
 
 SELECT '' AS twentyfour, p.f1, poly.f1, p.f1 <@ poly.f1 AS contained
-   FROM POLYGON_GEOM_TBL poly, POINT_GEOM_TBL p;
+   FROM POLYGON_TBL poly, POINT_TBL p;
 
 SELECT '' AS four, npoints(f1) AS npoints, f1 AS polygon
-   FROM POLYGON_GEOM_TBL;
+   FROM POLYGON_TBL;
 
 SELECT '' AS four, polygon(f1)
-   FROM BOX_GEOM_TBL;
+   FROM BOX_TBL;
 
 SELECT '' AS four, polygon(f1)
-   FROM PATH_GEOM_TBL WHERE isclosed(f1);
+   FROM PATH_TBL WHERE isclosed(f1);
 
 SELECT '' AS four, f1 AS open_path, polygon( pclose(f1)) AS polygon
-   FROM PATH_GEOM_TBL
+   FROM PATH_TBL
    WHERE isopen(f1);
 
-<<<<<<< HEAD
--- convert circles to polygons using the default number of points
-SELECT '' AS six, polygon(f1)
-   FROM CIRCLE_GEOM_TBL;
-
--- convert the circle to an 8-point polygon
-SELECT '' AS six, polygon(8, f1)
-   FROM CIRCLE_GEOM_TBL;
-=======
 -- To box
 SELECT f1, f1::box FROM POLYGON_TBL;
 
@@ -478,36 +393,26 @@ SELECT p1.f1, p2.f1 FROM POLYGON_TBL p1, POLYGON_TBL p2 WHERE p1.f1 |&> p2.f1;
 
 -- Distance to polygon
 SELECT p1.f1, p2.f1, p1.f1 <-> p2.f1 FROM POLYGON_TBL p1, POLYGON_TBL p2;
->>>>>>> 9e1c9f959422192bbe1b842a2a1ffaf76b080196
 
 --
 -- Circles
 --
 
 SELECT '' AS six, circle(f1, 50.0)
-   FROM POINT_GEOM_TBL;
+   FROM POINT_TBL;
 
 SELECT '' AS four, circle(f1)
-   FROM BOX_GEOM_TBL;
+   FROM BOX_TBL;
 
 SELECT '' AS two, circle(f1)
-   FROM POLYGON_GEOM_TBL
+   FROM POLYGON_TBL
    WHERE (# f1) >= 3;
 
 SELECT '' AS twentyfour, c1.f1 AS circle, p1.f1 AS point, (p1.f1 <-> c1.f1) AS distance
-   FROM CIRCLE_GEOM_TBL c1, POINT_GEOM_TBL p1
+   FROM CIRCLE_TBL c1, POINT_TBL p1
    WHERE (p1.f1 <-> c1.f1) > 0
    ORDER BY distance, area(c1.f1), p1.f1[0];
 
-<<<<<<< HEAD
--- Clean up GPDB-added tables
-DROP TABLE box_geom_tbl;
-DROP TABLE circle_geom_tbl;
-DROP TABLE lseg_geom_tbl;
-DROP TABLE path_geom_tbl;
-DROP TABLE polygon_geom_tbl;
-DROP TABLE point_geom_tbl;
-=======
 -- To polygon
 SELECT f1, f1::polygon FROM CIRCLE_TBL WHERE f1 >= '<(0,0),1>';
 
@@ -600,4 +505,3 @@ SELECT c.f1, p.f1, c.f1 / p.f1 FROM CIRCLE_TBL c, POINT_TBL p WHERE p.f1 ~= '(0,
 
 -- Distance to polygon
 SELECT c.f1, p.f1, c.f1 <-> p.f1 FROM CIRCLE_TBL c, POLYGON_TBL p;
->>>>>>> 9e1c9f959422192bbe1b842a2a1ffaf76b080196
