c89a6c1b27fc7fd9ead6b24127268655f03a7f2f
Heikki Linnakangas <hlinnakangas@pivotal.io>
2020-03-03 11:16:07 +0200

    Fix PARTITION BY START/END without EVERY.
    It means create one partition, with the START and END as the bounds.

--- src/backend/parser/parse_partition.c
+++ src/backend/parser/parse_partition.c
@@ -15,12 +15,16 @@
 
 #include "access/table.h"
 #include "catalog/pg_type.h"
+#include "commands/defrem.h"
 #include "nodes/makefuncs.h"
 #include "nodes/parsenodes.h"
 #include "parser/parse_utilcmd.h"
 #include "utils/lsyscache.h"
 #include "utils/partcache.h"
 #include "utils/rel.h"
+
+static CreateStmt *makePartitionCreateStmt(Relation parentrel, char *partname,
+										   PartitionBoundSpec *boundspec);
 
 /*
  * Create a list of CreateStmts, to create partitions based on 'gpPartSpec'
@@ -146,22 +150,55 @@ generatePartitions(CreateStmt *cstmt, Oid parentrelid, GpPartitionSpec *gpPartSp
 					elog(ERROR, "NULL not allowed in EVERY"); // GPDB_12_MERGE_FIXME: improve message
 				everyconsts = lappend(everyconsts, everyconst);
 			}
+
+			int start_int = DatumGetInt32(linitial_node(Const, startconsts)->constvalue);
+			int end_int = DatumGetInt32(linitial_node(Const, endconsts)->constvalue);
+			int every_int = DatumGetInt32(linitial_node(Const, everyconsts)->constvalue);
+
+			for (int bound = start_int; bound < end_int; bound += every_int)
+			{
+				PartitionBoundSpec *boundspec;
+				CreateStmt *childstmt;
+				char	   *partname;
+
+				boundspec = makeNode(PartitionBoundSpec);
+				boundspec->strategy = PARTITION_STRATEGY_RANGE;
+				boundspec->is_default = false;
+				boundspec->lowerdatums = list_make1(makeConst(INT4OID,
+															  -1,
+															  InvalidOid,
+															  sizeof(int32),
+															  Int32GetDatum(bound),
+															  false,
+															  true));
+				boundspec->upperdatums = list_make1(makeConst(INT4OID,
+															  -1,
+															  InvalidOid,
+															  sizeof(int32),
+															  Int32GetDatum(bound + every_int),
+															  false,
+															  true));
+				boundspec->location = -1;
+
+				/* GPDB_12_MERGE_FIXME: this way of choosing the partition rel name
+				 * is different from what it used to be.
+				 */
+				partname = psprintf("%s_%d", RelationGetRelationName(parentrel), bound);
+
+				childstmt = makePartitionCreateStmt(parentrel, partname, boundspec);
+
+				result = lappend(result, childstmt);
+			}
 		}
 		else
 		{
-			everyconsts = list_make1(makeConst(INT4OID, -1, InvalidOid, sizeof(int32),
-											   Int32GetDatum(1), false, true));
-		}
-
-		int start_int = DatumGetInt32(linitial_node(Const, startconsts)->constvalue);
-		int end_int = DatumGetInt32(linitial_node(Const, endconsts)->constvalue);
-		int every_int = DatumGetInt32(linitial_node(Const, everyconsts)->constvalue);
-
-		for (int bound = start_int; bound < end_int; bound += every_int)
-		{
+			/* Without EVERY, create just one partition that covers the whole range */
 			PartitionBoundSpec *boundspec;
 			CreateStmt *childstmt;
 			char	   *partname;
+
+			int start_int = DatumGetInt32(linitial_node(Const, startconsts)->constvalue);
+			int end_int = DatumGetInt32(linitial_node(Const, endconsts)->constvalue);
 
 			boundspec = makeNode(PartitionBoundSpec);
 			boundspec->strategy = PARTITION_STRATEGY_RANGE;
@@ -170,14 +207,14 @@ generatePartitions(CreateStmt *cstmt, Oid parentrelid, GpPartitionSpec *gpPartSp
 														  -1,
 														  InvalidOid,
 														  sizeof(int32),
-														  Int32GetDatum(bound),
+														  Int32GetDatum(start_int),
 														  false,
 														  true));
 			boundspec->upperdatums = list_make1(makeConst(INT4OID,
 														  -1,
 														  InvalidOid,
 														  sizeof(int32),
-														  Int32GetDatum(bound + every_int),
+														  Int32GetDatum(end_int),
 														  false,
 														  true));
 			boundspec->location = -1;
@@ -185,25 +222,9 @@ generatePartitions(CreateStmt *cstmt, Oid parentrelid, GpPartitionSpec *gpPartSp
 			/* GPDB_12_MERGE_FIXME: this way of choosing the partition rel name
 			 * is different from what it used to be.
 			 */
-			partname = psprintf("%s_%d", RelationGetRelationName(parentrel), bound);
+			partname = psprintf("%s_%d", RelationGetRelationName(parentrel), start_int);
 
-			childstmt = makeNode(CreateStmt);
-			childstmt->relation = makeRangeVar(schemaname, partname, -1);
-			childstmt->tableElts = NIL;
-			childstmt->inhRelations = list_make1(parentrv);
-			childstmt->partbound = boundspec;
-			childstmt->partspec = NULL;
-			childstmt->ofTypename = NULL;
-			childstmt->constraints = NIL;
-			childstmt->options = NIL; // FIXME: copy from parent stmt?
-			childstmt->oncommit = ONCOMMIT_NOOP;  // FIXME: copy from parent stmt?
-			childstmt->tablespacename = NULL;   // FIXME: copy from parent stmt?
-			childstmt->accessMethod = cstmt->accessMethod;
-			childstmt->if_not_exists = false;
-			childstmt->distributedBy = NULL; // FIXME: copy from parent stmt?
-			childstmt->partitionBy = NULL;
-			childstmt->relKind = 0;
-			childstmt->ownerid = cstmt->ownerid;
+			childstmt = makePartitionCreateStmt(parentrel, partname, boundspec);
 
 			result = lappend(result, childstmt);
 		}
@@ -214,4 +235,35 @@ generatePartitions(CreateStmt *cstmt, Oid parentrelid, GpPartitionSpec *gpPartSp
 	table_close(parentrel, NoLock);
 
 	return result;
+}
+
+static CreateStmt *
+makePartitionCreateStmt(Relation parentrel, char *partname, PartitionBoundSpec *boundspec)
+{
+	CreateStmt *childstmt;
+	RangeVar   *parentrv;
+	char	   *schemaname;
+
+	schemaname = get_namespace_name(parentrel->rd_rel->relnamespace);
+	parentrv = makeRangeVar(schemaname, RelationGetRelationName(parentrel), -1);
+
+	childstmt = makeNode(CreateStmt);
+	childstmt->relation = makeRangeVar(schemaname, partname, -1);
+	childstmt->tableElts = NIL;
+	childstmt->inhRelations = list_make1(parentrv);
+	childstmt->partbound = boundspec;
+	childstmt->partspec = NULL;
+	childstmt->ofTypename = NULL;
+	childstmt->constraints = NIL;
+	childstmt->options = NIL; // FIXME: copy from parent stmt?
+	childstmt->oncommit = ONCOMMIT_NOOP;  // FIXME: copy from parent stmt?
+	childstmt->tablespacename = NULL;   // FIXME: copy from parent stmt?
+	childstmt->accessMethod = get_am_name(parentrel->rd_rel->relam);
+	childstmt->if_not_exists = false;
+	childstmt->distributedBy = NULL; // FIXME: copy from parent stmt?
+	childstmt->partitionBy = NULL;
+	childstmt->relKind = 0;
+	childstmt->ownerid = parentrel->rd_rel->relowner;
+
+	return childstmt;
 }
