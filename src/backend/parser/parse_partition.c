c08f7518273826bab191701e7ea269fbb475bc57
Heikki Linnakangas <hlinnakangas@pivotal.io>
2020-03-04 00:10:00 +0200

    Add support for creating a DEFAULT partition with the old GPDB syntax.

--- src/backend/parser/parse_partition.c
+++ src/backend/parser/parse_partition.c
@@ -23,6 +23,15 @@
 #include "utils/partcache.h"
 #include "utils/rel.h"
 
+static List *generateRangePartitions(ParseState *pstate, CreateStmt *cstmt,
+									 Relation parentrel,
+									 GpPartitionElem *elem,
+									 const char *queryString);
+static List *generateDefaultPartition(ParseState *pstate, CreateStmt *cstmt,
+									  Relation parentrel,
+									  GpPartitionElem *elem,
+									  const char *queryString);
+
 static CreateStmt *makePartitionCreateStmt(Relation parentrel, char *partname,
 										   PartitionBoundSpec *boundspec);
 
@@ -37,168 +46,157 @@ generatePartitions(CreateStmt *cstmt, Oid parentrelid, GpPartitionSpec *gpPartSp
 	Relation	parentrel;
 	List	   *result = NIL;
 	char	   *schemaname;
-	RangeVar   *parentrv;
 	ListCell   *lc;
-	PartitionKey partkey;
 	ParseState *pstate;
 
 	pstate = make_parsestate(NULL);
 	pstate->p_sourcetext = queryString;
 
 	parentrel = table_open(parentrelid, NoLock);
-	partkey = RelationGetPartitionKey(parentrel);
 
 	schemaname = get_namespace_name(parentrel->rd_rel->relnamespace);
-
-	parentrv = makeRangeVar(schemaname, RelationGetRelationName(parentrel), -1);
 
 	foreach(lc, gpPartSpec->partElem)
 	{
 		GpPartitionElem *elem = lfirst_node(GpPartitionElem, lc);
-		GpPartitionBoundSpec *boundspec;
-		List	   *startconsts = NIL;
-		List	   *endconsts = NIL;
-		List	   *everyconsts = NIL;
+		List	   *new_parts;
 
-		if (!IsA(elem->boundSpec, GpPartitionBoundSpec))
-			elog(ERROR, "only range START/END/EVERY implemented");
-		boundspec = (GpPartitionBoundSpec *) elem->boundSpec;
-		Assert(boundspec);
-
-		if (partkey->partnatts != 1)
-			ereport(ERROR,
-					(errcode(ERRCODE_INVALID_TABLE_DEFINITION),
-					 errmsg("START/RANGE/EVERY is only supported for tables with a single partitioning key")));
-
-		/* Parse the START/END/EVERY clauses */
+		if (elem->boundSpec)
+			new_parts = generateRangePartitions(pstate, cstmt, parentrel, elem, queryString);
+		else if (elem->isDefault)
 		{
-			List	   *startvalues = boundspec->partStart;
-			ListCell   *lc;
-			int			keyidx;
-
-			if (list_length(startvalues) != partkey->partnatts)
-				elog(ERROR, "invalid number of start values"); // GPDB_12_MERGE_FIXME: improve message
-
-			keyidx = 0;
-			foreach(lc, startvalues)
-			{
-				Const	   *startconst;
-
-				startconst = transformPartitionBoundValue(pstate,
-														  lfirst(lc),
-														  "", /* GPDB_12_MERGE_FIXME: colname */
-														  get_partition_col_typid(partkey, keyidx),
-														  get_partition_col_typmod(partkey, keyidx),
-														  get_partition_col_collation(partkey, keyidx));
-				if (startconst->consttype != INT4OID)
-					elog(ERROR, "fixme: only int4 columns implemented");
-				if (startconst->constisnull)
-					elog(ERROR, "NULL not allowed in START"); // GPDB_12_MERGE_FIXME: improve message
-				startconsts = lappend(startconsts, startconst);
-			}
-		}
-		{
-			List	   *endvalues = boundspec->partEnd;
-			ListCell   *lc;
-			int			keyidx;
-
-			if (list_length(endvalues) != partkey->partnatts)
-				elog(ERROR, "invalid number of end values"); // GPDB_12_MERGE_FIXME: improve message
-
-			keyidx = 0;
-			foreach(lc, endvalues)
-			{
-				Const	   *endconst;
-
-				endconst = transformPartitionBoundValue(pstate,
-														lfirst(lc),
-														"", /* GPDB_12_MERGE_FIXME: colname */
-														get_partition_col_typid(partkey, keyidx),
-														get_partition_col_typmod(partkey, keyidx),
-														get_partition_col_collation(partkey, keyidx));
-				if (endconst->consttype != INT4OID)
-					elog(ERROR, "fixme: only int4 columns implemented");
-				if (endconst->constisnull)
-					elog(ERROR, "NULL not allowed in END"); // GPDB_12_MERGE_FIXME: improve message
-				endconsts = lappend(endconsts, endconst);
-			}
-		}
-
-		if (boundspec->partEvery)
-		{
-			List	   *everyvalues = boundspec->partEvery;
-			ListCell   *lc;
-			int			keyidx;
-
-			if (list_length(everyvalues) != partkey->partnatts)
-				elog(ERROR, "invalid number of every values"); // GPDB_12_MERGE_FIXME: improve message
-
-			keyidx = 0;
-			foreach(lc, everyvalues)
-			{
-				Const	   *everyconst;
-
-				everyconst = transformPartitionBoundValue(pstate,
-														  lfirst(lc),
-														  "", /* GPDB_12_MERGE_FIXME: colname */
-														  get_partition_col_typid(partkey, keyidx),
-														  get_partition_col_typmod(partkey, keyidx),
-														  get_partition_col_collation(partkey, keyidx));
-				if (everyconst->consttype != INT4OID)
-					elog(ERROR, "fixme: only int4 columns implemented");
-				if (everyconst->constisnull)
-					elog(ERROR, "NULL not allowed in EVERY"); // GPDB_12_MERGE_FIXME: improve message
-				everyconsts = lappend(everyconsts, everyconst);
-			}
-
-			int start_int = DatumGetInt32(linitial_node(Const, startconsts)->constvalue);
-			int end_int = DatumGetInt32(linitial_node(Const, endconsts)->constvalue);
-			int every_int = DatumGetInt32(linitial_node(Const, everyconsts)->constvalue);
-
-			for (int bound = start_int; bound < end_int; bound += every_int)
-			{
-				PartitionBoundSpec *boundspec;
-				CreateStmt *childstmt;
-				char	   *partname;
-
-				boundspec = makeNode(PartitionBoundSpec);
-				boundspec->strategy = PARTITION_STRATEGY_RANGE;
-				boundspec->is_default = false;
-				boundspec->lowerdatums = list_make1(makeConst(INT4OID,
-															  -1,
-															  InvalidOid,
-															  sizeof(int32),
-															  Int32GetDatum(bound),
-															  false,
-															  true));
-				boundspec->upperdatums = list_make1(makeConst(INT4OID,
-															  -1,
-															  InvalidOid,
-															  sizeof(int32),
-															  Int32GetDatum(bound + every_int),
-															  false,
-															  true));
-				boundspec->location = -1;
-
-				/* GPDB_12_MERGE_FIXME: this way of choosing the partition rel name
-				 * is different from what it used to be.
-				 */
-				partname = psprintf("%s_%d", RelationGetRelationName(parentrel), bound);
-
-				childstmt = makePartitionCreateStmt(parentrel, partname, boundspec);
-
-				result = lappend(result, childstmt);
-			}
+			new_parts = generateDefaultPartition(pstate, cstmt, parentrel, elem, queryString);
 		}
 		else
 		{
-			/* Without EVERY, create just one partition that covers the whole range */
+			/* GPDB_12_MERGE_FIXME: which cases are we not handling yet? */
+			elog(ERROR, "unexpected GpPartitionElem");
+		}
+
+		result = list_concat(result, new_parts);
+	}
+
+	free_parsestate(pstate);
+
+	table_close(parentrel, NoLock);
+
+	return result;
+}
+
+/* Generate partitions for START (..) END (..) EVERY (..) */
+List *
+generateRangePartitions(ParseState *pstate,
+						CreateStmt *cstmt, Relation parentrel,
+						GpPartitionElem *elem,
+						const char *queryString)
+{
+	GpPartitionBoundSpec *boundspec;
+	List	   *startconsts = NIL;
+	List	   *endconsts = NIL;
+	List	   *everyconsts = NIL;
+	List	   *result = NIL;
+	PartitionKey partkey;
+
+	boundspec = (GpPartitionBoundSpec *) elem->boundSpec;
+	Assert(boundspec);
+
+	partkey = RelationGetPartitionKey(parentrel);
+
+	if (partkey->partnatts != 1)
+		ereport(ERROR,
+				(errcode(ERRCODE_INVALID_TABLE_DEFINITION),
+				 errmsg("START/RANGE/EVERY is only supported for tables with a single partitioning key")));
+
+	/* Parse the START/END/EVERY clauses */
+	{
+		List	   *startvalues = boundspec->partStart;
+		ListCell   *lc;
+		int			keyidx;
+
+		if (list_length(startvalues) != partkey->partnatts)
+			elog(ERROR, "invalid number of start values"); // GPDB_12_MERGE_FIXME: improve message
+
+		keyidx = 0;
+		foreach(lc, startvalues)
+		{
+			Const	   *startconst;
+
+			startconst = transformPartitionBoundValue(pstate,
+													  lfirst(lc),
+													  "", /* GPDB_12_MERGE_FIXME: colname */
+													  get_partition_col_typid(partkey, keyidx),
+													  get_partition_col_typmod(partkey, keyidx),
+													  get_partition_col_collation(partkey, keyidx));
+			if (startconst->consttype != INT4OID)
+				elog(ERROR, "fixme: only int4 columns implemented");
+			if (startconst->constisnull)
+				elog(ERROR, "NULL not allowed in START"); // GPDB_12_MERGE_FIXME: improve message
+			startconsts = lappend(startconsts, startconst);
+		}
+	}
+	{
+		List	   *endvalues = boundspec->partEnd;
+		ListCell   *lc;
+		int			keyidx;
+
+		if (list_length(endvalues) != partkey->partnatts)
+			elog(ERROR, "invalid number of end values"); // GPDB_12_MERGE_FIXME: improve message
+
+		keyidx = 0;
+		foreach(lc, endvalues)
+		{
+			Const	   *endconst;
+
+			endconst = transformPartitionBoundValue(pstate,
+													lfirst(lc),
+													"", /* GPDB_12_MERGE_FIXME: colname */
+													get_partition_col_typid(partkey, keyidx),
+													get_partition_col_typmod(partkey, keyidx),
+													get_partition_col_collation(partkey, keyidx));
+			if (endconst->consttype != INT4OID)
+				elog(ERROR, "fixme: only int4 columns implemented");
+			if (endconst->constisnull)
+				elog(ERROR, "NULL not allowed in END"); // GPDB_12_MERGE_FIXME: improve message
+			endconsts = lappend(endconsts, endconst);
+		}
+	}
+
+	if (boundspec->partEvery)
+	{
+		List	   *everyvalues = boundspec->partEvery;
+		ListCell   *lc;
+		int			keyidx;
+
+		if (list_length(everyvalues) != partkey->partnatts)
+			elog(ERROR, "invalid number of every values"); // GPDB_12_MERGE_FIXME: improve message
+
+		keyidx = 0;
+		foreach(lc, everyvalues)
+		{
+			Const	   *everyconst;
+
+			everyconst = transformPartitionBoundValue(pstate,
+													  lfirst(lc),
+													  "", /* GPDB_12_MERGE_FIXME: colname */
+													  get_partition_col_typid(partkey, keyidx),
+													  get_partition_col_typmod(partkey, keyidx),
+													  get_partition_col_collation(partkey, keyidx));
+			if (everyconst->consttype != INT4OID)
+				elog(ERROR, "fixme: only int4 columns implemented");
+			if (everyconst->constisnull)
+				elog(ERROR, "NULL not allowed in EVERY"); // GPDB_12_MERGE_FIXME: improve message
+			everyconsts = lappend(everyconsts, everyconst);
+		}
+
+		int start_int = DatumGetInt32(linitial_node(Const, startconsts)->constvalue);
+		int end_int = DatumGetInt32(linitial_node(Const, endconsts)->constvalue);
+		int every_int = DatumGetInt32(linitial_node(Const, everyconsts)->constvalue);
+
+		for (int bound = start_int; bound < end_int; bound += every_int)
+		{
 			PartitionBoundSpec *boundspec;
 			CreateStmt *childstmt;
 			char	   *partname;
-
-			int start_int = DatumGetInt32(linitial_node(Const, startconsts)->constvalue);
-			int end_int = DatumGetInt32(linitial_node(Const, endconsts)->constvalue);
 
 			boundspec = makeNode(PartitionBoundSpec);
 			boundspec->strategy = PARTITION_STRATEGY_RANGE;
@@ -207,14 +205,14 @@ generatePartitions(CreateStmt *cstmt, Oid parentrelid, GpPartitionSpec *gpPartSp
 														  -1,
 														  InvalidOid,
 														  sizeof(int32),
-														  Int32GetDatum(start_int),
+														  Int32GetDatum(bound),
 														  false,
 														  true));
 			boundspec->upperdatums = list_make1(makeConst(INT4OID,
 														  -1,
 														  InvalidOid,
 														  sizeof(int32),
-														  Int32GetDatum(end_int),
+														  Int32GetDatum(bound + every_int),
 														  false,
 														  true));
 			boundspec->location = -1;
@@ -222,19 +220,77 @@ generatePartitions(CreateStmt *cstmt, Oid parentrelid, GpPartitionSpec *gpPartSp
 			/* GPDB_12_MERGE_FIXME: this way of choosing the partition rel name
 			 * is different from what it used to be.
 			 */
-			partname = psprintf("%s_%d", RelationGetRelationName(parentrel), start_int);
+			partname = psprintf("%s_%d", RelationGetRelationName(parentrel), bound);
 
 			childstmt = makePartitionCreateStmt(parentrel, partname, boundspec);
 
 			result = lappend(result, childstmt);
 		}
 	}
+	else
+	{
+		/* Without EVERY, create just one partition that covers the whole range */
+		PartitionBoundSpec *boundspec;
+		CreateStmt *childstmt;
+		char	   *partname;
 
-	free_parsestate(pstate);
+		int start_int = DatumGetInt32(linitial_node(Const, startconsts)->constvalue);
+		int end_int = DatumGetInt32(linitial_node(Const, endconsts)->constvalue);
 
-	table_close(parentrel, NoLock);
+		boundspec = makeNode(PartitionBoundSpec);
+		boundspec->strategy = PARTITION_STRATEGY_RANGE;
+		boundspec->is_default = false;
+		boundspec->lowerdatums = list_make1(makeConst(INT4OID,
+													  -1,
+													  InvalidOid,
+													  sizeof(int32),
+													  Int32GetDatum(start_int),
+													  false,
+													  true));
+		boundspec->upperdatums = list_make1(makeConst(INT4OID,
+													  -1,
+													  InvalidOid,
+													  sizeof(int32),
+													  Int32GetDatum(end_int),
+													  false,
+													  true));
+		boundspec->location = -1;
+
+		/* GPDB_12_MERGE_FIXME: this way of choosing the partition rel name
+		 * is different from what it used to be.
+		 */
+		partname = psprintf("%s_%d", RelationGetRelationName(parentrel), start_int);
+
+		childstmt = makePartitionCreateStmt(parentrel, partname, boundspec);
+
+		result = lappend(result, childstmt);
+	}
 
 	return result;
+}
+
+static List *
+generateDefaultPartition(ParseState *pstate, CreateStmt *cstmt,
+						 Relation parentrel,
+						 GpPartitionElem *elem,
+						 const char *queryString)
+{
+	PartitionBoundSpec *boundspec;
+	CreateStmt *childstmt;
+	char	   *partname;
+
+	boundspec = makeNode(PartitionBoundSpec);
+	boundspec->is_default = true;
+	boundspec->location = -1;
+
+	/* GPDB_12_MERGE_FIXME: this way of choosing the partition rel name
+	 * is different from what it used to be.
+	 */
+	partname = psprintf("%s_prt_default", RelationGetRelationName(parentrel));
+
+	childstmt = makePartitionCreateStmt(parentrel, partname, boundspec);
+
+	return list_make1(childstmt);
 }
 
 static CreateStmt *
