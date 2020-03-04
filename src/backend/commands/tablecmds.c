19207be501164f74668c6e90ffefb961ecf9661a
Heikki Linnakangas <hlinnakangas@pivotal.io>
2020-03-04 12:44:57 +0200

    Fix dispatching of ALTER TABLE ADD UNIQUE

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -8453,8 +8453,11 @@ ATExecAddIndex(AlteredTableInfo *tab, Relation rel,
 	Assert(stmt->transformed);
 
 	/* The index should already be built if we are a QE */
+	/* GPDB_12_MERGE_FIXME: it doesn't seem to work that way anymore. */
+#if 0
 	if (Gp_role == GP_ROLE_EXECUTE)
 		return InvalidObjectAddress;
+#endif
 
 	/* suppress schema rights check when rebuilding existing index */
 	check_rights = !is_rebuild;
