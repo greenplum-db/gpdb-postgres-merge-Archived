fd8f8e1b24d3d21a0fc2b5688cb97abc3a73b0bb
Heikki Linnakangas <hlinnakangas@pivotal.io>
2020-03-09 23:08:43 +0200

    Fix new test in 'join' test file to pass on GPDB.
    The planner needed some persuasion to choose the same plan as in upstream.

--- src/test/regress/sql/join.sql
+++ src/test/regress/sql/join.sql
@@ -1977,11 +1977,22 @@ drop table join_ut1;
 
 begin;
 
-create table fkest (a int, b int, c int unique, primary key(a,b));
+-- GPDB: persuade the planner to choose same plan as in upstream.
+set local random_page_cost=4;
+set local enable_nestloop=on;
+
+-- GPDB: in upstream, there's a unique index on 'c', but in GPDB you can't
+-- have two unique indexes with no columns in common. Create it as normal
+-- index instead, it doesn't affect the test.
+create table fkest (a int, b int, c int, primary key(a,b));
+create index fkest_c_key on fkest (c);
 create table fkest1 (a int, b int, primary key(a,b));
 
-insert into fkest select x/10, x%10, x from generate_series(1,1000) x;
-insert into fkest1 select x/10, x%10 from generate_series(1,1000) x;
+-- GPDB: insert 10x as much data as in upstream, to further persuade
+-- index scans. In GPDB, 1000 rows, as used in upstream test, fits in on
+-- just one page on each segment, because of the larger block size.
+insert into fkest select x/10, x%10, x from generate_series(1,1000*10) x;
+insert into fkest1 select x/10, x%10 from generate_series(1,1000*10) x;
 
 alter table fkest1
   add constraint fkest1_a_b_fkey foreign key (a,b) references fkest;
