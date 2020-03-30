5e2bf1bb416fd19b7bce5a2e5ba37472a8744e03
Heikki Linnakangas <hlinnakangas@pivotal.io>
2020-03-30 18:59:24 +0300

    Reimplement more of the legacy PARTITION BY START/END/EVERY syntax.
    We now support datatypes other than int4 again.

--- src/backend/parser/parse_partition.c
+++ src/backend/parser/parse_partition.c
@@ -14,12 +14,18 @@
 #include "postgres.h"
 
 #include "access/table.h"
+#include "catalog/pg_collation.h"
 #include "catalog/pg_type.h"
 #include "commands/defrem.h"
 #include "commands/tablecmds.h"
 #include "nodes/makefuncs.h"
+#include "nodes/nodeFuncs.h"
 #include "nodes/parsenodes.h"
+#include "optimizer/optimizer.h"
+#include "parser/parse_expr.h"
+#include "parser/parse_oper.h"
 #include "parser/parse_utilcmd.h"
+#include "utils/builtins.h"
 #include "utils/lsyscache.h"
 #include "utils/partcache.h"
 #include "utils/rel.h"
@@ -111,6 +117,287 @@ generatePartitions(CreateStmt *cstmt, Oid parentrelid, GpPartitionSpec *gpPartSp
 	return result;
 }
 
+/*
+ * Functions for iterating through all the partition bounds based on
+ * START/END/EVERY.
+ */
+typedef struct
+{
+	PartitionKeyData *partkey;
+	Datum		endVal;
+
+	ExprState   *plusexprstate;
+	ParamListInfo plusexpr_params;
+	EState	   *estate;
+
+	Datum		currStart;
+	Datum		currEnd;
+	bool		called;
+	bool		endReached;
+} PartEveryIterator;
+
+static PartEveryIterator *
+initPartEveryIterator(ParseState *pstate, PartitionKeyData *partkey, const char *part_col_name,
+					  Node *start, Node *end, Node *every)
+{
+	PartEveryIterator *iter;
+	Datum		startVal;
+	Datum		endVal;
+	Datum		everyVal;
+	Oid			plusop;
+	Oid			part_col_typid;
+	int32		part_col_typmod;
+	Oid			part_col_collation;
+
+	/* caller should've checked this already */
+	Assert(partkey->partnatts == 1);
+
+	part_col_typid = get_partition_col_typid(partkey, 0);
+	part_col_typmod = get_partition_col_typmod(partkey, 0);
+	part_col_collation = get_partition_col_collation(partkey, 0);
+
+	/* Parse the START/END/EVERY clauses */
+	if (start)
+	{
+		Const	   *startConst;
+
+		startConst = transformPartitionBoundValue(pstate,
+												  start,
+												  part_col_name,
+												  part_col_typid,
+												  part_col_typmod,
+												  part_col_collation);
+		if (startConst->constisnull)
+			elog(ERROR, "NULL not allowed in START"); // GPDB_12_MERGE_FIXME: improve message
+		startVal = startConst->constvalue;
+	}
+	else
+	{
+		if (part_col_typid != INT4OID)
+		{
+			// GPDB_12_MERGE_FIXME: support other types, and do this properly
+			elog(ERROR, "leaving out start value is only supported for integer column");
+		}
+		startVal = DatumGetInt32(INT_MAX);
+	}
+
+	if (end)
+	{
+		Const	   *endConst;
+
+		endConst = transformPartitionBoundValue(pstate,
+												end,
+												part_col_name,
+												part_col_typid,
+												part_col_typmod,
+												part_col_collation);
+		if (endConst->constisnull)
+			elog(ERROR, "NULL not allowed in END"); // GPDB_12_MERGE_FIXME: improve message
+		endVal = endConst->constvalue;
+	}
+	else
+	{
+		if (part_col_typid != INT4OID)
+		{
+			// GPDB_12_MERGE_FIXME: support other types, and do this properly
+			elog(ERROR, "leaving out end value is only supported for integer column");
+		}
+		endVal = DatumGetInt32(INT_MAX);
+	}
+
+	iter = palloc0(sizeof(PartEveryIterator));
+	iter->partkey = partkey;
+	iter->endVal = endVal;
+
+	if (every)
+	{
+		Node	   *plusexpr;
+		Param	   *param;
+
+		/*
+		 * NOTE: We don't use transformPartitionBoundValue() here. We don't want to cast
+		 * the EVERY clause to that type; rather, we'll be passing it to the + operator.
+		 * For example, if the partition column is a timestamp, the EVERY clause
+		 * can be an interval, so don't try to cast it to timestamp.
+		 */
+
+		param = makeNode(Param);
+		param->paramkind = PARAM_EXTERN;
+		param->paramid = 1;
+		param->paramtype = part_col_typid;
+		param->paramtypmod = part_col_typmod;
+		param->paramcollid = part_col_collation;
+		param->location = -1;
+
+		/* Look up + operator */
+		plusexpr = (Node *) make_op(pstate,
+									list_make2(makeString("pg_catalog"), makeString("+")),
+									(Node *) param,
+									(Node *) transformExpr(pstate, every, EXPR_KIND_PARTITION_BOUND),
+									pstate->p_last_srf,
+									-1);
+
+		/*
+		 * Check that the input expression's collation is compatible with one
+		 * specified for the parent's partition key (partcollation).  Don't throw
+		 * an error if it's the default collation which we'll replace with the
+		 * parent's collation anyway.
+		 */
+		if (IsA(plusexpr, CollateExpr))
+		{
+			Oid			exprCollOid = exprCollation(plusexpr);
+
+			if (OidIsValid(exprCollOid) &&
+				exprCollOid != DEFAULT_COLLATION_OID &&
+				exprCollOid != part_col_collation)
+				ereport(ERROR,
+						(errcode(ERRCODE_DATATYPE_MISMATCH),
+						 errmsg("collation of partition bound value for column \"%s\" does not match partition key collation \"%s\"",
+								part_col_name, get_collation_name(part_col_collation))));
+		}
+
+		plusexpr = coerce_to_target_type(pstate,
+										 plusexpr, exprType(plusexpr),
+										 part_col_typid,
+										 part_col_typmod,
+										 COERCION_ASSIGNMENT,
+										 COERCE_IMPLICIT_CAST,
+										 -1);
+		if (plusexpr == NULL)
+			ereport(ERROR,
+					(errcode(ERRCODE_DATATYPE_MISMATCH),
+					 errmsg("specified value cannot be cast to type %s for column \"%s\"",
+							format_type_be(part_col_typid), part_col_name)));
+
+		iter->estate = CreateExecutorState();
+
+		iter->plusexpr_params = makeParamList(1);
+		iter->plusexpr_params->params[0].value = (Datum) 0;
+		iter->plusexpr_params->params[0].isnull = true;
+		iter->plusexpr_params->params[0].pflags = 0;
+		iter->plusexpr_params->params[0].ptype = part_col_typid;
+
+		iter->estate->es_param_list_info = iter->plusexpr_params;
+
+		iter->plusexprstate = ExecInitExprWithParams((Expr *) plusexpr, iter->plusexpr_params);
+	}
+	else
+	{
+		everyVal = (Datum) 0;
+		plusop = InvalidOid;
+	}
+
+	iter->currEnd = startVal;
+	iter->currStart = (Datum) 0;
+	iter->called = false;
+	iter->endReached = false;
+
+	return iter;
+}
+
+static void
+freePartEveryIterator(PartEveryIterator *iter)
+{
+	if (iter->estate)
+		FreeExecutorState(iter->estate);
+}
+
+/*
+ * Return next partition bound in START/END/EVERY specification.
+ */
+static bool
+nextPartBound(PartEveryIterator *iter)
+{
+	bool		firstcall;
+
+	firstcall = !iter->called;
+	iter->called = true;
+
+	if (iter->plusexprstate)
+	{
+		/*
+		 * Call (previous bound) + EVERY
+		 */
+		Datum		next;
+		int32		cmpval;
+		bool		isnull;
+
+		/*
+		 * Corner case: if the previous end was equal to the END value, we're done.
+		 * Normally, we would catch it after calling the + operator, but it's possible
+		 * that we would overflow first.
+		 */
+		if (iter->endReached)
+			return false;
+
+		iter->plusexpr_params->params[0].isnull = false;
+		iter->plusexpr_params->params[0].value = iter->currEnd;
+
+		next = ExecEvalExprSwitchContext(iter->plusexprstate,
+										 GetPerTupleExprContext(iter->estate),
+										 &isnull);
+		if (isnull)
+			elog(ERROR, "plus-operator returned NULL"); // GPDB_12_MERGE_FIXME: better message
+
+		/* Is the next bound greater than END? */
+		cmpval = DatumGetInt32(FunctionCall2Coll(&iter->partkey->partsupfunc[0],
+												 iter->partkey->partcollation[0],
+												 next,
+												 iter->endVal));
+		if (cmpval > 0)
+			return false;
+		if (cmpval == 0)
+			iter->endReached = true;
+
+		/*
+		 * Sanity check that the next bound is > previous bound. This prevents us
+		 * from getting into an infinite loop if the + operator is not behaving.
+		 */
+		cmpval = DatumGetInt32(FunctionCall2Coll(&iter->partkey->partsupfunc[0],
+												 iter->partkey->partcollation[0],
+												 iter->currEnd,
+												 next));
+		if (cmpval >= 0)
+		{
+			if (firstcall)
+			{
+				/*
+				 * Second iteration: parameter hasn't increased the
+				 * current end from the old end.
+				 */
+				ereport(ERROR,
+						(errcode(ERRCODE_INVALID_TABLE_DEFINITION),
+						 errmsg("EVERY parameter too small")));
+			}
+			else
+			{
+				/*
+				 * We got a smaller value but later than we
+				 * thought so it must be an overflow.
+				 */
+				ereport(ERROR,
+						(errcode(ERRCODE_INVALID_TABLE_DEFINITION),
+						 errmsg("END parameter not reached before type overflows")));
+			}
+		}
+
+		iter->currStart = iter->currEnd;
+		iter->currEnd = next;
+		return true;
+	}
+	else
+	{
+		/* Without EVERY, create just one partition that covers the whole range */
+		if (iter->called)
+			return false;
+
+		iter->called = true;
+		iter->currStart = iter->currEnd;
+		iter->currEnd = iter->endVal;
+		return true;
+	}
+}
+
 /* Generate partitions for START (..) END (..) EVERY (..) */
 static List *
 generateRangePartitions(ParseState *pstate,
@@ -120,11 +407,13 @@ generateRangePartitions(ParseState *pstate,
 						int *num_unnamed_parts_p)
 {
 	GpPartitionBoundSpec *boundspec;
-	List	   *startconsts = NIL;
-	List	   *endconsts = NIL;
-	List	   *everyconsts = NIL;
 	List	   *result = NIL;
 	PartitionKey partkey;
+	char	   *partcolname;
+	PartEveryIterator *boundIter;
+	Node	   *start;
+	Node	   *end = NULL;
+	Node	   *every = NULL;
 
 	boundspec = (GpPartitionBoundSpec *) elem->boundSpec;
 	Assert(boundspec);
@@ -136,183 +425,58 @@ generateRangePartitions(ParseState *pstate,
 				(errcode(ERRCODE_INVALID_TABLE_DEFINITION),
 				 errmsg("START/RANGE/EVERY is only supported for tables with a single partitioning key")));
 
-	/* Parse the START/END/EVERY clauses */
+	/* not sure if the syntax allows this, but better safe than sorry */
+	if (partkey->partattrs[0] == 0)
+		ereport(ERROR,
+				(errcode(ERRCODE_INVALID_TABLE_DEFINITION),
+				 errmsg("START/END/EVERY not supported when partition key is an expression")));
+	partcolname = NameStr(TupleDescAttr(RelationGetDescr(parentrel), partkey->partattrs[0] - 1)->attname);
+
+	if (list_length(boundspec->partStart) != partkey->partnatts)
+		elog(ERROR, "invalid number of start values"); // GPDB_12_MERGE_FIXME: improve message
+	start = linitial(boundspec->partStart);
+
+	if (boundspec->partEnd)
 	{
-		List	   *startvalues = boundspec->partStart;
-		ListCell   *lc;
-		int			keyidx;
-
-		if (list_length(startvalues) != partkey->partnatts)
-			elog(ERROR, "invalid number of start values"); // GPDB_12_MERGE_FIXME: improve message
-
-		keyidx = 0;
-		foreach(lc, startvalues)
-		{
-			Const	   *startconst;
-
-			startconst = transformPartitionBoundValue(pstate,
-													  lfirst(lc),
-													  "", /* GPDB_12_MERGE_FIXME: colname */
-													  get_partition_col_typid(partkey, keyidx),
-													  get_partition_col_typmod(partkey, keyidx),
-													  get_partition_col_collation(partkey, keyidx));
-			if (startconst->consttype != INT4OID)
-				elog(ERROR, "fixme: only int4 columns implemented");
-			if (startconst->constisnull)
-				elog(ERROR, "NULL not allowed in START"); // GPDB_12_MERGE_FIXME: improve message
-			startconsts = lappend(startconsts, startconst);
-		}
-	}
-	{
-		List	   *endvalues = boundspec->partEnd;
-		ListCell   *lc;
-		int			keyidx;
-
-		if (list_length(endvalues) == 0)
-		{
-			/* the partition spans till infinity */
-			foreach(lc, startconsts)
-			{
-				Const	   *startconst = lfirst_node(Const, lc);
-				Const	   *endconst;
-
-				if (startconst->consttype != INT4OID)
-				{
-					// GPDB_12_MERGE_FIXME: improve message
-					elog(ERROR, "leaving out end value is only supported for integer column");
-				}
-
-				endconst = makeConst(INT4OID,
-									 -1,
-									 InvalidOid,
-									 sizeof(int32),
-									 DatumGetInt32(INT_MAX),
-									 false,
-									 true);
-
-				endconsts = lappend(endconsts, endconst);
-			}
-		}
-		else
-		{
-			if (list_length(endvalues) != partkey->partnatts)
-				elog(ERROR, "invalid number of end values"); // GPDB_12_MERGE_FIXME: improve message
-
-			keyidx = 0;
-			foreach(lc, endvalues)
-			{
-				Const	   *endconst;
-
-				endconst = transformPartitionBoundValue(pstate,
-														lfirst(lc),
-														"", /* GPDB_12_MERGE_FIXME: colname */
-														get_partition_col_typid(partkey, keyidx),
-														get_partition_col_typmod(partkey, keyidx),
-														get_partition_col_collation(partkey, keyidx));
-				if (endconst->consttype != INT4OID)
-					elog(ERROR, "fixme: only int4 columns implemented");
-				if (endconst->constisnull)
-					elog(ERROR, "NULL not allowed in END"); // GPDB_12_MERGE_FIXME: improve message
-				endconsts = lappend(endconsts, endconst);
-			}
-		}
+		if (list_length(boundspec->partEnd) != partkey->partnatts)
+			elog(ERROR, "invalid number of end values"); // GPDB_12_MERGE_FIXME: improve message
+		end = linitial(boundspec->partEnd);
 	}
 
 	if (boundspec->partEvery)
 	{
-		List	   *everyvalues = boundspec->partEvery;
-		ListCell   *lc;
-		int			keyidx;
-
-		if (list_length(everyvalues) != partkey->partnatts)
+		if (list_length(boundspec->partEvery) != partkey->partnatts)
 			elog(ERROR, "invalid number of every values"); // GPDB_12_MERGE_FIXME: improve message
-
-		keyidx = 0;
-		foreach(lc, everyvalues)
-		{
-			Const	   *everyconst;
-
-			everyconst = transformPartitionBoundValue(pstate,
-													  lfirst(lc),
-													  "", /* GPDB_12_MERGE_FIXME: colname */
-													  get_partition_col_typid(partkey, keyidx),
-													  get_partition_col_typmod(partkey, keyidx),
-													  get_partition_col_collation(partkey, keyidx));
-			if (everyconst->consttype != INT4OID)
-				elog(ERROR, "fixme: only int4 columns implemented");
-			if (everyconst->constisnull)
-				elog(ERROR, "NULL not allowed in EVERY"); // GPDB_12_MERGE_FIXME: improve message
-			everyconsts = lappend(everyconsts, everyconst);
-		}
-
-		int start_int = DatumGetInt32(linitial_node(Const, startconsts)->constvalue);
-		int end_int = DatumGetInt32(linitial_node(Const, endconsts)->constvalue);
-		int every_int = DatumGetInt32(linitial_node(Const, everyconsts)->constvalue);
-
-		for (int bound = start_int; bound < end_int; bound += every_int)
-		{
-			PartitionBoundSpec *boundspec;
-			CreateStmt *childstmt;
-			char	   *partname;
-
-			boundspec = makeNode(PartitionBoundSpec);
-			boundspec->strategy = PARTITION_STRATEGY_RANGE;
-			boundspec->is_default = false;
-			boundspec->lowerdatums = list_make1(makeConst(INT4OID,
-														  -1,
-														  InvalidOid,
-														  sizeof(int32),
-														  Int32GetDatum(bound),
-														  false,
-														  true));
-			boundspec->upperdatums = list_make1(makeConst(INT4OID,
-														  -1,
-														  InvalidOid,
-														  sizeof(int32),
-														  Int32GetDatum(bound + every_int),
-														  false,
-														  true));
-			boundspec->location = -1;
-
-			/*
-			 * GPDB_12_MERGE_FIXME: this way of choosing the partition rel name
-			 * isn't exactly the same as it used to be.
-			 * And we probably should be using ChooseRelationName() here.
-			 */
-			partname = psprintf("%s_1_prt_%d", RelationGetRelationName(parentrel), ++(*num_unnamed_parts_p));
-
-			childstmt = makePartitionCreateStmt(parentrel, partname, boundspec);
-
-			result = lappend(result, childstmt);
-		}
+		every = linitial(boundspec->partEvery);
 	}
-	else
+
+	partkey = RelationGetPartitionKey(parentrel);
+
+	boundIter = initPartEveryIterator(pstate, partkey, partcolname, start, end, every);
+
+	while (nextPartBound(boundIter))
 	{
-		/* Without EVERY, create just one partition that covers the whole range */
 		PartitionBoundSpec *boundspec;
 		CreateStmt *childstmt;
 		char	   *partname;
 
-		int start_int = DatumGetInt32(linitial_node(Const, startconsts)->constvalue);
-		int end_int = DatumGetInt32(linitial_node(Const, endconsts)->constvalue);
-
 		boundspec = makeNode(PartitionBoundSpec);
 		boundspec->strategy = PARTITION_STRATEGY_RANGE;
 		boundspec->is_default = false;
-		boundspec->lowerdatums = list_make1(makeConst(INT4OID,
-													  -1,
-													  InvalidOid,
-													  sizeof(int32),
-													  Int32GetDatum(start_int),
+		boundspec->lowerdatums = list_make1(makeConst(boundIter->partkey->parttypid[0],
+													  boundIter->partkey->parttypmod[0],
+													  boundIter->partkey->parttypcoll[0],
+													  boundIter->partkey->parttyplen[0],
+													  boundIter->currStart,
 													  false,
-													  true));
-		boundspec->upperdatums = list_make1(makeConst(INT4OID,
-													  -1,
-													  InvalidOid,
-													  sizeof(int32),
-													  Int32GetDatum(end_int),
+													  boundIter->partkey->parttypbyval[0]));
+		boundspec->upperdatums = list_make1(makeConst(boundIter->partkey->parttypid[0],
+													  boundIter->partkey->parttypmod[0],
+													  boundIter->partkey->parttypcoll[0],
+													  boundIter->partkey->parttyplen[0],
+													  boundIter->currEnd,
 													  false,
-													  true));
+													  boundIter->partkey->parttypbyval[0]));
 		boundspec->location = -1;
 
 		/*
@@ -320,12 +484,14 @@ generateRangePartitions(ParseState *pstate,
 		 * isn't exactly the same as it used to be.
 		 * And we probably should be using ChooseRelationName() here.
 		 */
-		partname = psprintf("%s_prt_%d", RelationGetRelationName(parentrel), start_int);
+		partname = psprintf("%s_1_prt_%d", RelationGetRelationName(parentrel), ++(*num_unnamed_parts_p));
 
 		childstmt = makePartitionCreateStmt(parentrel, partname, boundspec);
 
 		result = lappend(result, childstmt);
 	}
+
+	freePartEveryIterator(boundIter);
 
 	return result;
 }
