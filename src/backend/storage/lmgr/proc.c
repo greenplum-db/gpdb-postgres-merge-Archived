6e103d0c8d6567aff456c4d8e0879b7a2e9e732a
Weinan Wang <wewang@pivotal.io>
2020-04-03 14:26:28 +0800

    Merge remote-tracking branch 'offical/master' into iteration_REL_12

--- src/backend/storage/lmgr/proc.c
+++ src/backend/storage/lmgr/proc.c
@@ -991,11 +991,12 @@ ProcKill(int code, Datum arg)
 	/*
 	 * Remove the shared snapshot slot.
 	 */
-	if (SharedSnapshot.desc != NULL)
+	if (SharedLocalSnapshotSlot != NULL)
 	{
 		if (Gp_role == GP_ROLE_DISPATCH)
 		{
-			SharedSnapshotRemove("Query Dispatcher");
+			SharedSnapshotRemove(SharedLocalSnapshotSlot,
+								 "Query Dispatcher");
 		}
 	    else if (IS_QUERY_DISPATCHER() && Gp_role == GP_ROLE_EXECUTE && !Gp_is_writer)
 	    {
@@ -1005,9 +1006,10 @@ ProcKill(int code, Datum arg)
 	    }
 		else if (Gp_role == GP_ROLE_EXECUTE && Gp_is_writer)
 		{
-			SharedSnapshotRemove("Writer qExec");
+			SharedSnapshotRemove(SharedLocalSnapshotSlot,
+								 "Writer qExec");
 		}
-		SharedSnapshot.desc = NULL;
+		SharedLocalSnapshotSlot = NULL;
 	}
 
 	SyncRepCleanupAtProcExit();
