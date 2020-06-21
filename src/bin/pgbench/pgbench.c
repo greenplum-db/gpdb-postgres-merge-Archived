30a74963dcf0553dd8b36d0dde3b1763c9f0c22e
Heikki Linnakangas <hlinnakangas@pivotal.io>
2020-06-21 23:08:14 +0300

    Fix pgbench --tablespace option.
    The CREATE TABLE commands constructed in pgbench had the DISTRIBUTED BY
    and TABLESPACE options the wrong way 'round, so that you got a syntax
    error. For example:
    
    $ pgbench postgres -i --tablespace "pg_default"
    creating tables...
    ERROR:  syntax error at or near "tablespace"
    LINE 1: ...22)) with (appendonly=false) DISTRIBUTED BY (bid) tablespace...
                                                                 ^
    Put the clauses in right order.
    
    We have no test coverage for this at the moment, but PostgreSQL v11 adds
    a test for this (commit ed8a7c6fcf). I noticed this while looking at test
    failures with the PostgreSQL v12 merge.
    
    (cherry-pick of https://github.com/greenplum-db/gpdb/pull/10342)

--- src/bin/pgbench/pgbench.c
+++ src/bin/pgbench/pgbench.c
@@ -3676,12 +3676,12 @@ initCreateTables(PGconn *con)
 		opts[0] = '\0';
 		if (ddl->declare_fillfactor)
 			snprintf(opts + strlen(opts), sizeof(opts) - strlen(opts),
-					 " with (fillfactor=%d, %s)", fillfactor, storage_clause);
+					 " with (fillfactor=%d, %s)",
+					 fillfactor, storage_clause);
 		else
 			snprintf(opts + strlen(opts), sizeof(opts) - strlen(opts),
-					 " with (%s)", storage_clause);
-		snprintf(opts + strlen(opts), sizeof(opts) - strlen(opts),
-				 " DISTRIBUTED BY (%s)", ddl->distributed_col);
+					 " with (%s)",
+					 storage_clause);
 		if (tablespace != NULL)
 		{
 			char	   *escape_tablespace;
@@ -3692,6 +3692,9 @@ initCreateTables(PGconn *con)
 					 " tablespace %s", escape_tablespace);
 			PQfreemem(escape_tablespace);
 		}
+		snprintf(opts + strlen(opts), sizeof(opts) - strlen(opts),
+				 " distributed by (%s)",
+				 ddl->distributed_col);
 
 		cols = (scale >= SCALE_32BIT_THRESHOLD) ? ddl->bigcols : ddl->smcols;
 
