greenplum-db/gpdb-postgres-merge-Archived

commit 3e09f4cb73e128204d7960445a78a1612784f609

Fix the logic to determinate if it is a appendoptimized relation
Access method OID is not the right way to determine if we are dealing
with an AO relation.  See commit 9809bcae17 for more details.

More such cases may be lingering, they can be fixed as a follow up.

Co-authored-by: Georgios Kokolatos <gkokolatos@pivotal.io>

src/backend/commands/tablecmds.c
@@ -5902,7 +5902,7 @@ ATAocsWriteNewColumns(AlteredTableInfo *tab)
 	}
 
 	rel = heap_open(tab->relid, NoLock);
-	Assert(rel->rd_rel->relam == AOCO_TABLE_AM_OID);
+	Assert(RelationIsAoCols(rel));
 
 	/* Try to recycle any old segfiles first. */
 	AppendOnlyRecycleDeadSegments(rel);
@@ -7420,7 +7420,7 @@ ATExecAddColumn(List **wqueue, AlteredTableInfo *tab, Relation rel,
 				AlteredTableInfo *childtab;
 				childtab = ATGetQueueEntry(wqueue, rel);
 
-				if (rel->rd_rel->relam == AOCO_TABLE_AM_OID)
+				if (RelationIsAoCols(rel))
 					childtab->rewrite |= AT_REWRITE_NEW_COLUMNS_ONLY_AOCS;
 				heap_close(rel, NoLock);
 			}

src/backend/catalog/aoblkdir.c
@@ -35,26 +35,18 @@ AlterTableCreateAoBlkdirTable(Oid relOid)
 	Oid			classObjectId[3];
 	int16		coloptions[3];
 	List	   *indexColNames;
+	bool	   isAO;
 
 	SIMPLE_FAULT_INJECTOR("before_acquire_lock_during_create_ao_blkdir_table");
 
 	/*
 	 * Check if this is an appendoptimized table, without acquiring any lock.
-	 *
-	 * GPDB_12_MERGE_FIXME: we need a new API such as get_rel_am() for this in
-	 * lsyscache.c.  Similar boilerplate code also exists in DefineIndex.
 	 */
-	{
-		bool isAO;
-		HeapTuple tuple;
-		tuple = SearchSysCache1(RELOID, ObjectIdGetDatum(relOid));
-		Form_pg_class pgc = (Form_pg_class) GETSTRUCT(tuple);
-		isAO = (pgc->relam == APPENDOPTIMIZED_TABLE_AM_OID ||
-				pgc->relam == AOCO_TABLE_AM_OID);
-		ReleaseSysCache(tuple);
-		if (!isAO)
-			return;
-	}
+	rel = table_open(relOid, NoLock);
+	isAO = RelationIsAppendOptimized(rel);
+	table_close(rel, NoLock);
+	if (!isAO)
+		return;
 
 	/*
 	 * GPDB_12_MERGE_FIXME: Block directory creation must block any

commit 9e0358d7bf52b649b34d4d6c4378a20235e26390

Properly fix the refactor in 3e09f4cb73
The commit 3e09f4cb73 tried to move the swap operation for
pg_appendonly entries to the appendoptimized table AM iplementation
for CLUSTER command.  However, that is not correct because swapping of
pg_appendonly entries is a generic operation, needed by commands such
as CLUSTER, REFRESH MATERIALIZED VIEW and ALTER TABLE commands that
rewrite the relation.  Therefore, reinstate the call to
SwapAppendonlyEntries to where the pg_class entries are swapped.

This change passess the tests that 3e09f4cb73 caused to fail.

Co-authored-by: Georgios Kokolatos <gkokolatos@pivotal.io>

src/backend/access/aocs/aocsam_handler.c
@@ -1234,8 +1234,6 @@ aoco_relation_copy_for_cluster(Relation OldHeap, Relation NewHeap,
 
 	/* Finish and deallocate insertion */
 	aocs_insert_finish(idesc);
-
-	SwapAppendonlyEntries(RelationGetRelid(OldHeap), RelationGetRelid(NewHeap));
 }
 
 static bool

src/backend/access/appendonly/appendonlyam_handler.c
@@ -1116,8 +1116,6 @@ appendonly_relation_copy_for_cluster(Relation OldHeap, Relation NewHeap,
 
 	/* Finish and deallocate insertion */
 	appendonly_insert_finish(aoInsertDesc);
-
-	SwapAppendonlyEntries(RelationGetRelid(OldHeap), RelationGetRelid(NewHeap));
 }
 
 static bool

src/backend/catalog/pg_appendonly.c
@@ -547,10 +547,10 @@ GetAppendEntryForMove(
 							  NULL, 1, key);
 	tuple = systable_getnext(scan);
 	if (!HeapTupleIsValid(tuple))
-		ereport(ERROR,
-				(errcode(ERRCODE_UNDEFINED_OBJECT),
-				 errmsg("appendonly table relid \"%d\" does not exist in "
-						"pg_appendonly", relId)));
+	{
+		systable_endscan(scan);
+		return tuple;
+	}
 
     *aosegrelid = heap_getattr(tuple,
 							   Anum_pg_appendonly_segrelid,
@@ -598,7 +598,6 @@ SwapAppendonlyEntries(Oid entryRelId1, Oid entryRelId2)
 {
 	Relation	pg_appendonly_rel;
 	TupleDesc	pg_appendonly_dsc;
-	HeapTuple	tuple;
 	HeapTuple	tupleCopy1;
 	HeapTuple	tupleCopy2;
 	Datum 		*newValues;
@@ -614,7 +613,7 @@ SwapAppendonlyEntries(Oid entryRelId1, Oid entryRelId2)
 	pg_appendonly_rel = table_open(AppendOnlyRelationId, RowExclusiveLock);
 	pg_appendonly_dsc = RelationGetDescr(pg_appendonly_rel);
 	
-	tuple = GetAppendEntryForMove(
+	tupleCopy1 = GetAppendEntryForMove(
 							pg_appendonly_rel,
 							pg_appendonly_dsc,
 							entryRelId1,
@@ -622,15 +621,7 @@ SwapAppendonlyEntries(Oid entryRelId1, Oid entryRelId2)
 							&aoblkdirrelid1,
 							&aovisimaprelid1);
 
-	/* Since gp_fastsequence entry is referenced by aosegrelid, it rides along  */
-
-	/*
-	 * Delete the appendonly table entry from the catalog (pg_appendonly).
-	 */
-	tupleCopy1 = heap_copytuple(tuple);
-	simple_heap_delete(pg_appendonly_rel, &tuple->t_self);
-	
-	tuple = GetAppendEntryForMove(
+	tupleCopy2 = GetAppendEntryForMove(
 							pg_appendonly_rel,
 							pg_appendonly_dsc,
 							entryRelId2,
@@ -638,13 +629,17 @@ SwapAppendonlyEntries(Oid entryRelId1, Oid entryRelId2)
 							&aoblkdirrelid2,
 							&aovisimaprelid2);
 
-	/* Since gp_fastsequence entry is referenced by aosegrelid, it rides along  */
+	if (!HeapTupleIsValid(tupleCopy1) || !HeapTupleIsValid(tupleCopy2))
+	{
+		if (HeapTupleIsValid(tupleCopy1) || HeapTupleIsValid(tupleCopy2))
+			elog(ERROR, "swapping pg_appendonly entries is not permitted for non-appendoptimized tables");
+		table_close(pg_appendonly_rel, NoLock);
+		return;
+	}
 
-	/*
-	 * Delete the appendonly table entry from the catalog (pg_appendonly).
-	 */
-	tupleCopy2 = heap_copytuple(tuple);
-	simple_heap_delete(pg_appendonly_rel, &tuple->t_self);
+	/* Since gp_fastsequence entry is referenced by aosegrelid, it rides along  */
+	simple_heap_delete(pg_appendonly_rel, &tupleCopy1->t_self);
+	simple_heap_delete(pg_appendonly_rel, &tupleCopy2->t_self);
 
 	/*
 	 * (Re)insert.

src/backend/commands/cluster.c
@@ -1270,6 +1270,8 @@ swap_relation_files(Oid r1, Oid r2, bool target_is_pg_class,
 		relform2->relallvisible = swap_allvisible;
 	}
 
+	SwapAppendonlyEntries(r1, r2);
+
 	/*
 	 * Update the tuples in pg_class --- unless the target relation of the
 	 * swap is pg_class itself.  In that case, there is zero point in making

src/backend/commands/tablecmds.c
@@ -6377,9 +6377,6 @@ ATRewriteTable(AlteredTableInfo *tab, Oid OIDNewHeap, LOCKMODE lockmode)
 
 		table_finish_bulk_insert(newrel, ti_options);
 
-		if (RelationIsAppendOptimized(newrel))
-			SwapAppendonlyEntries(tab->relid, OIDNewHeap);
-
 		table_close(newrel, NoLock);
 	}
 }