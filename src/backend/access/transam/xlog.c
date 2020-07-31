218321518560e1d6bf2fde1ffd14c367356bdc78
Ashwin Agrawal <aashwin@vmware.com>
2020-07-31 16:52:14 -0700

    Avoid closing the DTM redo file during checkpoint record replay
    This was causing incorrect file pointer during write of redo record on
    standby during forced promotion. Issue was exposed by
    gpactivatestandby behave test. Code looks similar on master, I am
    really puzzled why we are seeing the issue on merge branch and not on
    master. But irrespective its incorrect to close the file here as
    someone might have opened it before us and closing invalidates the
    previous callers file pointer invalid.

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -6152,7 +6152,11 @@ XLogProcessCheckpointRecord(XLogReaderState *rec)
 		/* Handle the DTX information. */
 		UtilityModeFindOrCreateDtmRedoFile();
 		redoDtxCheckPoint(ckptExtended.dtxCheckpoint);
-		UtilityModeCloseDtmRedoFile();
+		/*
+		 * Avoid closing the file here as possibly the file was already open
+		 * and above call didn't really open it.  Hence closing the same here
+		 * is incorrect.
+		 */
 	}
 }
 
