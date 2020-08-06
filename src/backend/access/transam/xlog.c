e10892b1a50ce9f47453721f087300d15aab69a6
Asim R P <pasim@vmware.com>
2020-08-06 17:47:33 +0530

    Let oldest xmin on hot standby advance as expected
    Greenplum's logic to compute oldest xmin includes distributed
    transaction ID (rather, the mapped local xmin).  On a hot standby /
    mirror, distributed oldest xmin cannot be advanced because QD never
    dispatches queries to mirrors.  This commit avoids initialization of
    distributed log subsystem on mirrors.
    
    This change makes the GUC hot_standby_feedback behave like it does in
    upstream and passes 001_stream_repl.pl TAP test.  Without this change,
    GetOldestXmin would keep returning the same xmin, which was assigned
    to DistributedLogShared->oldestXmin.

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -7121,8 +7121,17 @@ StartupXLOG(void)
 			 */
 			StartupCLOG();
 			StartupSUBTRANS(oldestActiveXID);
-			DistributedLog_Startup(oldestActiveXID,
-								   XidFromFullTransactionId(ShmemVariableCache->nextFullXid));
+			/*
+			 * Do not initialize DistributedLog subsystem. Hot standby /
+			 * mirror cannot advance distributed xmin because QD does not
+			 * dispatch queries to mirrors.  To align with upstream, we still
+			 * want a functional hot standby as far as a single primary/mirror
+			 * pair is concerned.  Initializing distributed log subsystem
+			 * affects oldest xmin computation in hot standby, the oldest xmin
+			 * never advances.  Therefore, avoid initializing distributed log
+			 * in hot standby.  If, in future, queries from QD need to be
+			 * dispatched to mirrors, this will have to change.
+			 */
 
 			/*
 			 * If we're beginning at a shutdown checkpoint, we know that
