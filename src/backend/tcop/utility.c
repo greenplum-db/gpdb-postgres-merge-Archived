d2fee3a98f0f9daec19de5757a011521d991d34f
Heikki Linnakangas <hlinnakangas@pivotal.io>
2020-08-10 15:08:51 +0300

    Forbid PREPARE TRANSCATION completely.
    It was already forbidden in normal mode.
    
    It was causing the 009_twophase test to fail badly, because PREPARE
    TRANSACTION succeeded, but after that recovery panicked, because the
    user-supplied prepared transaction name didn't follow the expected
    "%u-%u" format that GPDB uses for all distributed transactions. Now
    that we forbid it altogether, the test will of course still fail, but
    at least recovery will finish so that the subsequent tests are executed.

--- src/backend/tcop/utility.c
+++ src/backend/tcop/utility.c
@@ -475,12 +475,16 @@ standard_ProcessUtility(PlannedStmt *pstmt,
 						break;
 
 					case TRANS_STMT_PREPARE:
-						if (Gp_role == GP_ROLE_DISPATCH)
-						{
-							ereport(ERROR, (errcode(ERRCODE_GP_COMMAND_ERROR),
-									errmsg("PREPARE TRANSACTION is not yet supported in Greenplum Database")));
-
-						}
+						/*
+						 * PREPARE TRANSACTION is not supported in GPDB. GPDB
+						 * uses prepared transactions to maintain consistency
+						 * across nodes, but we don't support the user to
+						 * initiate them.
+						 */
+						ereport(ERROR,
+								(errcode(ERRCODE_GP_COMMAND_ERROR),
+								 errmsg("PREPARE TRANSACTION is not yet supported in Greenplum Database")));
+#if 0
 						PreventCommandDuringRecovery("PREPARE TRANSACTION");
 						if (!PrepareTransactionBlock(stmt->gid))
 						{
@@ -488,6 +492,7 @@ standard_ProcessUtility(PlannedStmt *pstmt,
 							if (completionTag)
 								strcpy(completionTag, "ROLLBACK");
 						}
+#endif
 						break;
 
 					case TRANS_STMT_COMMIT_PREPARED:
