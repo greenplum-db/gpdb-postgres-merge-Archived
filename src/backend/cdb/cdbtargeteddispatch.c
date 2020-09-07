7dd76656f4f52415d119497f5fa9335e58dd53a1
Asim R P <pasim@vmware.com>
2020-09-07 17:56:38 +0530

    Remove FIXMEs that are no longer relevant
    A lot of code churn has taken place on the iteration branch since
    these fixmes were added.  The regression tests create_table_like,
    ao_locks are now passing without the additional AccessShareLock
    workaround.
    
    The failure of isolation2 tests uao/parallel_delete and
    uao/parallel_update brought these fixmes under the scanner.  With Orca
    turned on, the tests used to fail because the AccessShareLock, as
    expected in the answer file, was not found to be held when a DELETE
    statement was executed on AO table.  The lock was found to be relased
    by the relation_close() call fixed by this patch.

--- src/backend/cdb/cdbtargeteddispatch.c
+++ src/backend/cdb/cdbtargeteddispatch.c
@@ -118,20 +118,7 @@ GetContentIdsFromPlanForSingleRelation(PlannerInfo *root, Plan *plan, int rangeT
 	if (rte->rtekind == RTE_RELATION)
 	{
 		/* Get a copy of the rel's GpPolicy from the relcache. */
-		/*
-		 * GPDB_12_MERGE_FIXME:
-		 *
-		 * Before the merge, we used NoLock here, but the merge introduced
-		 * assertions into relation_open() that check that if it's called
-		 * with NoLock, the backend must already hold a lock. We were
-		 * hitting that assertion in the 'create_table_like' regression test,
-		 * when selecting from a table with an inheritance parent. I don't
-		 * understand why; we really should be holding a lock on all
-		 * tables being referenced by the plan, since we're still in the
-		 * planner. Investigate, fix, and revert this to NoLock.
-		 * Also change back the equivelant relation_close() call further down.
-		 */
-		relation = relation_open(rte->relid, AccessShareLock /* Was: NoLock */);
+		relation = relation_open(rte->relid, NoLock);
 		policy = relation->rd_cdbpolicy;
 
 		if (policy != NULL)
@@ -274,8 +261,7 @@ GetContentIdsFromPlanForSingleRelation(PlannerInfo *root, Plan *plan, int rangeT
 
 	if (rte->rtekind == RTE_RELATION)
 	{
-		/* GPDB_12_MERGE_FIXME: don't forget to change this also back to NoLock */
-		relation_close(relation, AccessShareLock);
+		relation_close(relation, NoLock);
 	}
 
 	result.haveProcessedAnyCalculations = true;
