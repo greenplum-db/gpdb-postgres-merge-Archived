b00a5cf950900a706b09b5a48ae2110468e20a1a
Ning Yu <nyu@pivotal.io>
2020-09-11 14:26:00 +0800

    Be more verbose in isolation2::gdd/dml_locks_only_targeted_table_in_query
    In the test we check the # of locks in the pg_locks, we used to only
    show a "1" per lock, which is enough to meet the testing purpose, but
    does not contain useful information for debugging on failures.
    
    Now we show the "locktype", "mode", and "granted", instead of the "1".

--- src/test/isolation2/sql/gdd/dml_locks_only_targeted_table_in_query.sql
+++ src/test/isolation2/sql/gdd/dml_locks_only_targeted_table_in_query.sql
@@ -9,27 +9,27 @@ INSERT INTO part_tbl_upd_del SELECT i, 1, i FROM generate_series(1,10)i;
 1: BEGIN;
 1: DELETE FROM part_tbl_upd_del;
 -- on QD, there's a lock on the root and the target partition
-1: SELECT 1 FROM pg_locks WHERE relation='part_tbl_upd_del_1_prt_1'::regclass::oid AND gp_segment_id = -1;
-1: SELECT 1 FROM pg_locks WHERE relation='part_tbl_upd_del'::regclass::oid AND gp_segment_id = -1;
+1: SELECT locktype, mode, granted FROM pg_locks WHERE relation='part_tbl_upd_del_1_prt_1'::regclass::oid AND gp_segment_id = -1;
+1: SELECT locktype, mode, granted FROM pg_locks WHERE relation='part_tbl_upd_del'::regclass::oid AND gp_segment_id = -1;
 1: ROLLBACK;
 
 
 1: BEGIN;
 1: UPDATE part_tbl_upd_del SET c = 1 WHERE c = 1;
 -- on QD, there's a lock on the root and the target partition
-1: SELECT 1 FROM pg_locks WHERE relation='part_tbl_upd_del_1_prt_1'::regclass::oid AND gp_segment_id = -1;
-1: SELECT 1 FROM pg_locks WHERE relation='part_tbl_upd_del'::regclass::oid AND gp_segment_id = -1;
+1: SELECT locktype, mode, granted FROM pg_locks WHERE relation='part_tbl_upd_del_1_prt_1'::regclass::oid AND gp_segment_id = -1;
+1: SELECT locktype, mode, granted FROM pg_locks WHERE relation='part_tbl_upd_del'::regclass::oid AND gp_segment_id = -1;
 1: ROLLBACK;
 
 1: BEGIN;
 1: DELETE FROM part_tbl_upd_del_1_prt_1;
 -- since the delete operation is on leaf part, there will be a lock on QD
-1: SELECT 1 FROM pg_locks WHERE relation='part_tbl_upd_del_1_prt_1'::regclass::oid AND gp_segment_id = -1 AND mode='RowExclusiveLock';
+1: SELECT locktype, mode, granted FROM pg_locks WHERE relation='part_tbl_upd_del_1_prt_1'::regclass::oid AND gp_segment_id = -1 AND mode='RowExclusiveLock';
 1: ROLLBACK;
 
 
 1: BEGIN;
 1: UPDATE part_tbl_upd_del_1_prt_1 SET c = 1 WHERE c = 1;
 -- since the update operation is on leaf part, there will be a lock on QD
-1: SELECT 1 FROM pg_locks WHERE relation='part_tbl_upd_del_1_prt_1'::regclass::oid AND gp_segment_id = -1 AND mode='RowExclusiveLock';
+1: SELECT locktype, mode, granted FROM pg_locks WHERE relation='part_tbl_upd_del_1_prt_1'::regclass::oid AND gp_segment_id = -1 AND mode='RowExclusiveLock';
 1: ROLLBACK;
