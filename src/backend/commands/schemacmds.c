8db3210325b56be941745520a4dc3c0684145585
Adam Lee <adlee@vmware.com>
2022-10-12 15:02:39 +0800

    Fix conflicts in schemacmds.c
    I believe the SetUserIdAndSecContext() should be called before
    NamespaceCreate() as the upstream always is.

--- src/backend/commands/schemacmds.c
+++ src/backend/commands/schemacmds.c
@@ -163,8 +163,19 @@ CreateSchemaCommand(CreateSchemaStmt *stmt, const char *queryString,
 							schemaName)));
 			return InvalidOid;
 		}
-<<<<<<< HEAD
 	}
+
+	/*
+	 * If the requested authorization is different from the current user,
+	 * temporarily set the current user so that the object(s) will be created
+	 * with the correct ownership.
+	 *
+	 * (The setting will be restored at the end of this routine, or in case of
+	 * error, transaction abort will clean things up.)
+	 */
+	if (saved_uid != owner_uid)
+		SetUserIdAndSecContext(owner_uid,
+							   save_sec_context | SECURITY_LOCAL_USERID_CHANGE);
 
 	/* Create the schema's namespace */
 	if (shouldDispatch || Gp_role != GP_ROLE_EXECUTE)
@@ -199,21 +210,7 @@ CreateSchemaCommand(CreateSchemaStmt *stmt, const char *queryString,
 	else
 	{
 		namespaceId = NamespaceCreate(schemaName, owner_uid, false);
-=======
->>>>>>> 7cd0d523d2581895e65cd0ebebc7e50caa8bbfda
 	}
-
-	/*
-	 * If the requested authorization is different from the current user,
-	 * temporarily set the current user so that the object(s) will be created
-	 * with the correct ownership.
-	 *
-	 * (The setting will be restored at the end of this routine, or in case of
-	 * error, transaction abort will clean things up.)
-	 */
-	if (saved_uid != owner_uid)
-		SetUserIdAndSecContext(owner_uid,
-							   save_sec_context | SECURITY_LOCAL_USERID_CHANGE);
 
 	/* Advance cmd counter to make the namespace visible */
 	CommandCounterIncrement();
